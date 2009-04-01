f7327edc325f4fbc7cad0a71ed2ad99befc6292f
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-04-02 01:23:20 +0200

    Added real, working Scala jUnit test

---
+++ codadris.scalaBot/src/ScalaBotTest.scala
@@ -0,0 +1,69 @@
+// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+
+package scalabot.tests
+
+import junit.framework._
+import junit.framework.Assert.assertEquals 
+import junit.framework.Assert.fail 
+import junit.textui._
+
+class Test1 extends TestCase {
+
+	var commit: Commit = null
+	
+	override def setUp {
+		commit = null
+	}
+	
+	def testCommitCreation = {
+		try {
+			commit = new Commit("","")
+			fail
+		} catch {
+			case a: Throwable => {
+			}
+		}
+	}
+	
+	def testCommitCreation1 = {
+		try {
+			commit = new Commit("") 
+			fail
+		} catch {
+			case a: Throwable => {
+			}
+		}
+		assert( commit == null )
+	}
+	
+	def testCommitCreation2 = {
+		commit = new Commit("Something")
+		assert( commit != null )	
+	}
+	
+	def testCommitCreation3 = {
+		commit = new Commit("Something", "notMaster")
+		assert( commit != null )
+	}	
+		
+		// try {
+		// 	fail()
+		// } catch {
+		// 	case e: IllegalArgumentException => // expected
+		// }
+	
+}
+
+object ScalaBotTest extends TestCase {
+	
+	def main (args: Array[String]) = {
+		TestRunner.run(suite)
+	}
+
+	def suite: Test = {
+		val suite = new TestSuite
+		suite.addTestSuite(classOf[Test1])
+		suite
+	}
+	
+} 
