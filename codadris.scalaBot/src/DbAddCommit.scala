39388ef40b4eee71defe16437e126d5665843bb1
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-04-02 01:26:25 +0200

    Added Db related stub Actors

---
+++ codadris.scalaBot/src/DbAddCommit.scala
@@ -0,0 +1,16 @@
+// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+
+package scalabot
+
+import scala.actors._
+
+
+object DbAddCommit {
+	
+	val debug = ScalaBot.debug
+	
+	def main(args: Array[String]) = {
+		println("*** Adding new commit to objDb: " + args)
+	}
+
+}
---
+++ codadris.scalaBot/src/DbListener.scala
@@ -0,0 +1,14 @@
+// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+
+package scalaBot
+
+import scala.actors._
+
+class DbListener extends Actor {
+	
+	override def act = {
+		Actor.loop {
+			
+		}
+	}
+}
