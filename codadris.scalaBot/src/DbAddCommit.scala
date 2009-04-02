e153c1c6f2e686c2ba511b45efd0fa46bb79c96d
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-04-02 04:58:01 +0200

    Added debug check for DbAddCommit, fixed stupid mistakes

--- codadris.scalaBot/src/DbAddCommit.scala
+++ codadris.scalaBot/src/DbAddCommit.scala
@@ -11,34 +11,26 @@ object DbAddCommit {
 	
 	val debug = ScalaBot.debug
 	
-	def writeCommitToDataBase(args: Map[String,Boolean]) = {
+	def writeCommitToDataBase(arg: Map[String,Boolean]) = {
 		val odb = ODBFactory.open("../ScalaBotCommitDataBase.neodatis")
-		args.foreach{ oneOf => 
-			odb.store( oneOf )
-		}
-			// writing Sha1 to odb
+		odb.store( arg )
+		if (debug)
+			println("*** writeCommitToDatabase: " + arg )
 		// query.addExtension("datanucleus.query.flushBeforeExecution","true");
 		odb.close
 	}
 	
 	def main(args: Array[String]) = {
-		println("*** Adding new commit list to objDb: " + args(0) + " to " + args(1))
-		// git-rev-list
+		if (debug)
+			println("*** Reqested adding new commit list to objDb: " + args(0) + " to " + args(1))
 		val command = Array("git", "rev-list", args(0) + "..." + args(1))
 		val output: String = CommandExec.cmdExec(command)
-		var listOfSha1 = List.fromString(output, ' ')
-		println(listOfSha1)
-		
+		var listOfSha1 = List.fromString(output, '\n')
 		listOfSha1.foreach{ oneOf => 
 			val x: Map[String,Boolean] = Map( oneOf -> true )
 			writeCommitToDataBase( x )	// sha1, show?
 		}
-			
-		// val inputStream = p.getInputStream
-		// val brCleanUp = new BufferedReader(new InputStreamReader (inputStream))
-
-		println("Done")
-		
+		println("Done. All ok")
 	}
 
 }
