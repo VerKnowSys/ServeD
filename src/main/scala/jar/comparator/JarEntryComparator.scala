8f59005e5b0063987101f28d8ccfe3231fe77570
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-06-26 01:22:08 +0200

    Moved jar comparator from codadris.utils to project.tools

---
+++ src/main/scala/jar/comparator/JarEntryComparator.scala
@@ -0,0 +1,78 @@
+// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+// This Software is a close code project. You may not redistribute this code without permission of author.
+
+package jar.comparator
+
+import _root_.java.util.jar.{JarEntry, JarFile}
+
+/**
+ * User: dmilith
+ * Date: Jun 18, 2009
+ * Time: 8:53:22 PM
+ */
+
+case class JarFileFields( val name_and_path: String, val crc: Long)
+
+class JarEntryComparator {
+	
+	private var p_elements = List[JarFileFields]()
+	private var p_elements2 = List[JarFileFields]()
+
+	val ignoreList = List( "pom.properties", "MANIFEST.MF", "VERKNOWS.SF" )
+
+	def add(t: JarFileFields) = p_elements = p_elements ::: List(t) ::: Nil
+
+	def add2(t: JarFileFields) = p_elements2 = p_elements2 ::: List(t) ::: Nil
+
+	def elements = p_elements
+
+	def elements2 = p_elements2
+
+	def diff = p_elements2 -- p_elements
+
+	def diff_? = if (diff == List() && size == size2) false else true
+	           
+	def size = p_elements size
+
+	def size2 = p_elements2 size
+
+	def load(file: String, file2: String) = {
+		val jarFile = new JarFile(file)
+		val jarFile2 = new JarFile(file2)
+		val entries = jarFile entries
+		val entries2 = jarFile2 entries
+		
+		while (entries.hasMoreElements) {
+			entries.nextElement match {
+				case ne: JarEntry => {
+					if (!ne.isDirectory) {
+						ignoreList.foreach( element =>
+								if (ne.getName.contains(element)) {
+									println("Ignoring " + element)
+								} else {
+									add(new JarFileFields(ne.getName, ne.getCrc))
+								}
+							)
+					}
+				}
+			}
+        }
+		while (entries2.hasMoreElements) {
+			entries2.nextElement match {
+				case ne: JarEntry => {
+					if (!ne.isDirectory) {
+						ignoreList.foreach( element =>
+								if (ne.getName.contains(element)) {
+									println("Ignoring " + element)
+								} else {
+									add2(new JarFileFields(ne.getName, ne.getCrc))
+								}
+							)
+					}
+				}
+			}
+        }
+	}
+	
+
+}
