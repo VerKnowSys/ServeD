212bb6224257dd92ba1e6b4dcd236f084b88b966
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-07-06 02:15:22 +0200

    Added dependency on scala-swing (for DeployerGUI)

---
+++ src/main/scala/deployer/DeployerGUI.scala
@@ -0,0 +1,22 @@
+// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+// This Software is a close code project. You may not redistribute this code without permission of author.
+
+import java.awt.{Dimension, Rectangle}
+import swing.{MainFrame, Frame, SimpleGUIApplication}
+
+/**
+ * User: dmilith
+ * Date: Jul 6, 2009
+ * Time: 1:33:54 AM
+ */
+
+
+object DeployerGUI extends SimpleGUIApplication {
+
+	def top = new MainFrame {
+		title = "Deployer GUI"
+		size = new Dimension(400,600)
+		visible = true
+	}
+
+}
