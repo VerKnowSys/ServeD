dcc73f49c69d1471c05613ac86ef6d16f99921e3
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-07-06 02:17:08 +0200

    Moved DeployerGUI to deployer package

--- src/main/scala/deployer/DeployerGUI.scala
+++ src/main/scala/deployer/DeployerGUI.scala
@@ -1,6 +1,8 @@
 // © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
 // This Software is a close code project. You may not redistribute this code without permission of author.
 
+package deployer
+
 import java.awt.{Dimension, Rectangle}
 import swing.{MainFrame, Frame, SimpleGUIApplication}
 
@@ -16,7 +18,6 @@ object DeployerGUI extends SimpleGUIApplication {
 	def top = new MainFrame {
 		title = "Deployer GUI"
 		size = new Dimension(400,600)
-		visible = true
 	}
 
 }
