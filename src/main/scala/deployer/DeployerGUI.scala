294f4ed1c04c983cacfa04abc62cf30b6859439e
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-07-09 15:04:23 +0200

    Added autoDetectRequirements method which automatically finds git and jarsigner executables in paths

--- src/main/scala/deployer/DeployerGUI.scala
+++ src/main/scala/deployer/DeployerGUI.scala
@@ -4,6 +4,9 @@
 package deployer
 
 import java.awt.Dimension
+import java.io.{OutputStreamWriter, File}
+import java.util.regex.Pattern
+import java.util.{NoSuchElementException, ArrayList}
 import org.apache.log4j.{ConsoleAppender, Level, PatternLayout, Logger}
 
 import prefs.Preferences
@@ -19,7 +22,52 @@ import swing.{MainFrame, Frame, SimpleGUIApplication}
 object DeployerGUI extends SimpleGUIApplication {
 
 	private val logger = Logger.getLogger(DeployerGUI.getClass)
-	private val prefs = (new Preferences).loadPreferences
+	private val prefs = new Preferences
+	private val searchIn = Array(
+				new File("/opt/local/bin/"), // XXX hardcoded paths
+				new File("/bin/"),
+				new File("/usr/bin/"),
+				new File("/usr/local/bin/")
+	)
+	private val requirements = Array(
+			("git", "gitExecutable"),
+			("jarsigner", "jarSignerExecutable")
+	)
+
+	def autoDetectRequirements = {
+		logger.info("Detecting requirements")
+		for (i <- 0 until requirements.size)
+			if (!(new File(prefs.get(requirements(i)._2)).exists)) {
+				val al = new ArrayList[File]()
+				if (System.getProperty("os.name").contains("Linux") ||
+					System.getProperty("os.name").contains("Mac")) {
+					for (path <- searchIn) {
+						if (path.exists) {
+							Deployer.findFile( path, new P {
+								override
+								def accept(t: String): Boolean = {
+									val fileRegex = ".*" + requirements(i)._1 + "$"
+									val pattern = Pattern.compile(fileRegex)
+									val mat = pattern.matcher(t)
+									if ( mat.find ) return true
+									return false
+								}
+							}, al)
+						}
+					}
+					try {
+						prefs.value.update(requirements(i)._2, al.toArray.first.toString)
+					} catch {
+						case x: NoSuchElementException => {
+							logger.error(requirements(i)._1 + " executable not found")
+						}
+					}
+				} else {
+					logger.error("Windows hosts not yet supported")
+					exit(1)
+				}
+			}
+	}
 
 	def top = new MainFrame {
 		Deployer.initLogger
@@ -28,6 +76,13 @@ object DeployerGUI extends SimpleGUIApplication {
 		}
 		title = "Deployer GUI"
 		size = new Dimension(600,750)
+		prepare
+	}
+
+	def prepare = {
+		autoDetectRequirements
+		logger.warn(prefs.get("gitExecutable"))
+		logger.warn(prefs.get("jarSignerExecutable"))
 	}
 
 
