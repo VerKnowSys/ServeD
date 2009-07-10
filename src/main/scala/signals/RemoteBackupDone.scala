dbc847f710eef4123780fa1706a142a80fe58404
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-07-10 09:57:36 +0200

    Added two more signals, they will be used soon (probably)

---
+++ src/main/scala/signals/RemoteBackupDone.scala
@@ -0,0 +1,12 @@
+// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+// This Software is a close code project. You may not redistribute this code without permission of author.
+
+package signals
+
+/**
+ * User: dmilith
+ * Date: Jul 10, 2009
+ * Time: 9:40:31 AM
+ */
+
+case object RemoteBackupDone
