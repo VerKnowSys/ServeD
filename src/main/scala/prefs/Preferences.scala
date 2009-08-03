b466fdd0ace2ecc1b866b00694470834a179620d
Daniel (dmilith) Dettlaff <dmilith@gmail.com>
2009-08-03 23:43:27 +0200

    Added one more additional dependency to Preferences, updated pom (added external maven2 repository)

--- src/main/scala/prefs/Preferences.scala
+++ src/main/scala/prefs/Preferences.scala
@@ -74,7 +74,8 @@ class Preferences(configFileNameInput: String) extends Utils {
 			"scala-compiler-2.7.5.jar",
 			"scala-library-2.7.5.jar",
 			"org.quaqua.swing-layout-5.4.1.jar",
-			"org.quaqua-5.4.1.jar"
+			"org.quaqua-5.4.1.jar",
+			"substance-5.1.jar"
 		),
 		"webstartArgumentsJVM" -> List(
 		  	"-Dcom.apple.macos.useScreenMenuBar=true",
