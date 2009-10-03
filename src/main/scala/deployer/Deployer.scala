26d8f9ac70bafdaa32b448dad9538ec76ba6237d
Daniel (dmilith) Dettlaff <dmilith@drakor.eu>
2009-10-04 00:26:55 +0200

    Huge a bit refactor, to make code more clear and less mutable.

--- src/main/scala/deployer/Deployer.scala
+++ src/main/scala/deployer/Deployer.scala
@@ -26,71 +26,11 @@ import utils.Utils
 
 
 object Deployer extends Actor with Utils {
+	
+	var prefs: Preferences = null
 
-	private val filesToBeDeployed = new ArrayList[File]()
-	private val uuid = UUID.randomUUID.toString
-	private val deployTmpDir = "/tmp/deployer-" + uuid + "/"
-	private val pathToMaven2Repo = System.getProperty("user.home") + "/.m2/repository/"
-	override
-	def logger = Logger.getLogger(Deployer.getClass)
+	override def logger = Logger.getLogger(Deployer.getClass)
 	initLogger
-	private var prefs = new Preferences // default file name
-	private val debug = prefs.getb("debug")
-	if (debug) {
-		setLoggerLevelDebug(Level.TRACE)
-	}
-	private var basicOnly_? = prefs.getb("deployOnlyBasicFiles")
-	private val basic_jar_names = prefs.getl("deployFilesBasic")
-	private val dependency_jar_names = prefs.getl("deployFilesAdditionalDependencies")
-	private val codebaseLocalDir = System.getProperty("user.home") + "/" + prefs.get("directoryForLocalDeploy")
-	private var macAppDeploy = false
-
-
-	def getFilesFromMavenRepositoryAndSignThem = {
-		logger.warn("Searching for jars in Maven repository and signing them..")
-		var jar_names = List[String]()
-		if (basicOnly_?) {
-			jar_names ++= basic_jar_names
-			logger.warn("Selected only basic jars to deploy")
-		} else {
-			jar_names ++= basic_jar_names ++ dependency_jar_names
-			logger.warn("Selected basic and dependendant jars to deploy")
-		}
-		jar_names.foreach { file =>
-			logger.info("*" + file + "*")
-			findFile(new File(pathToMaven2Repo), new P {
-				override
-				def accept(t: String): Boolean = {
-					val fileRegex = ".*" + file + "$"
-					val pattern = Pattern.compile(fileRegex)
-					val mat = pattern.matcher(t)
-					if ( mat.find ) {
-						if (!macAppDeploy)
-							signJar(t)
-						else { // don't sign jars when it's mac-app deploy
-							logger.warn("Copying jar: " + t.split("/").last)
-							FileUtils.copyFileToDirectory(new File(t), new File(deployTmpDir)) // copy files to temporary dir
-						}
-						return true
-					}
-					return false
-				}
-			}, filesToBeDeployed)
-		}
-	}
-
-
-	def signJar(fileToBeSigned: String) = {
-		new File(deployTmpDir).mkdir // make temporary place for jars before signinig
-		logger.warn("Signing jar: " + fileToBeSigned.split("/").last)
-		FileUtils.copyFileToDirectory(new File(fileToBeSigned), new File(deployTmpDir)) // copy files to temporary dir
-		val signCommand = Array(
-			prefs.get("jarSignerExecutable"), "-storepass", prefs.get("jarSignerPassword"),
-			deployTmpDir + fileToBeSigned.split("/").last,	prefs.get("jarSignerKeyName")
-			)
-		logger.debug(CommandExec.cmdExec(signCommand).trim)
-	}
-
 
 	override
 	def act = {
@@ -108,96 +48,44 @@ object Deployer extends Actor with Utils {
 	}
 
 
-	def backupLocal = {
-		val backupDate = (new Date).toString.replaceAll(" |:", "_")
-		val backupDir = new File(codebaseLocalDir + "../OLD_DEPLOYS/OLD_LOCAL_DEPLOY_" +  backupDate + "/")
-		val localDeployDir = new File (codebaseLocalDir)
-		backupDir.mkdir
-		localDeployDir.mkdir
-		logger.warn("Copying files from " + localDeployDir + " to " + backupDir)
-		FileUtils.copyDirectory(localDeployDir, backupDir)
-	}
+	def main(args: Array[String]) = {
 
-
-	def deployLocal = {
-		backupLocal
-		filesToBeDeployed.toArray foreach {
-			localFile => {
-				val comparator = new JarEntryComparator
-				comparator.load(deployTmpDir + localFile.toString.split("/").last, codebaseLocalDir + "lib/" + localFile.toString.split("/").last)
-				if (comparator.diff_?) {
-					logger.warn("File DIFFERENT: " + localFile.toString.split("/").last)
-					FileUtils.copyFileToDirectory( new File(deployTmpDir + localFile.toString.split("/").last), new File(codebaseLocalDir + "lib/"))
-				} else {
-					logger.warn("File IDENTICAL: " + localFile.toString.split("/").last)
-				}
-			}
-		}
-		logger.warn("Generating JNLP file")
-		var arguments = ""
-		for( i <- prefs.getl("webstartArgumentsJVM")) {
-			arguments += i + " "
-		}
-		val jnlp = new JNLPSkeleton(
-			prefs.get("jnlpMainClass"),
-			prefs.get("jnlpAppName"),
-			"file://" + codebaseLocalDir,
-			prefs.get("jnlpFileName"),
-			(prefs.getl("deployFilesBasic") ++ prefs.getl("deployFilesAdditionalDependencies")),
-			arguments,
-			prefs.get("jnlpVendor"),
-			"file://" + codebaseLocalDir,
-			prefs.get("jnlpIcon"),
-			prefs.get("jnlpDescription")
-			)
-		val tempJnlpFileName = "/tmp/" + prefs.get("jnlpFileName") 
-		jnlp.saveJNLP(tempJnlpFileName)
-		logger.warn("Putting JNLP file to local deploy dir")
-		FileUtils.copyFileToDirectory(new File(tempJnlpFileName), new File(codebaseLocalDir))
-	}
-
-
-	def copyAndInstallMacApp = {
-		logger.warn("Copying jars to temporary mac app")
-		FileUtils.copyDirectory(new File(deployTmpDir), new File(codebaseLocalDir + "../Coviob.app/lib")) // XXX: hardcoded Coviob.app name 
-		// XXX: application skeleton must be uploaded to ~/.codadris before deploy
-		logger.warn("Copying Application to /Applications")
-		FileUtils.copyDirectory(new File(codebaseLocalDir + "../Coviob.app"), new File("/Applications/Coviob.app"))
-		logger.warn("Making executable of app starting script..")
-		CommandExec.cmdExec(Array("chmod", "777", "/Applications/Coviob.app/Contents/MacOS/coviob2")) // XXX: why the fuck copying files will result changing permissions to files?!
-	}
-
-
-	def deployerHelp = logger.warn("\n\nDeployer quick help:\nValid params:\n" +
-								"\tlocal -> for basic local deploy\n" +
-								"\tlocal-full -> for full local deploy\n" +
-								"\twindows-app -> for Windows application deploy\n" +
-								"\tlinux-app -> for Linux (distro independent) application deploy\n" +
-								"\tmac-app -> for Mac OS X application deploy and install\n" +
-								"\tfull -> for full remote deploy\n" +
-								"\tTo run remote deploy, no arguments (defaults based on xml config).")
-
-
-	def main(args: Array[String]) {
+		lazy val filesToBeDeployed = new ArrayList[File]()
+		lazy val uuid = UUID.randomUUID.toString
+		lazy val deployTmpDir = "/tmp/deployer-" + uuid + "/"
+		lazy val pathToMaven2Repo = System.getProperty("user.home") + "/.m2/repository/"
 
 		addShutdownHook {
 			SSHActor ! Quit
 			Deployer ! Quit
 			logger.warn("Done.")
 		}
+
 		if (args.size == 0) {
 			logger.error("Missing argument: (config filename)")
 			exit
 		}
+
 		prefs = new Preferences(args(0))
+		val debug = prefs.getb("debug")
+		if (debug) {
+			setLoggerLevelDebug(Level.TRACE)
+		}
+
+		lazy val basic_jar_names = prefs.getl("deployFilesBasic")
+		lazy val dependency_jar_names = prefs.getl("deployFilesAdditionalDependencies")
+		lazy val codebaseLocalDir = System.getProperty("user.home") + "/" + prefs.get("directoryForLocalDeploy")
+		var macAppDeploy = false
+		var basicOnly_? = prefs.getb("deployOnlyBasicFiles")
+
 		logger.warn("Starting Deployer..")
-		logger.warn("Deployer home dir: " + System.getProperty("user.home") + "/.codadris/" )
+		logger.warn("Deployer home dir: " + System.getProperty("user.home") + "/.codadris/")
 		logger.warn("Maven 2 Repository dir: " + pathToMaven2Repo)
 		logger.warn("Deploy tmp dir: " + deployTmpDir)
 		logger.warn("Will deploy files to remote host to: " + prefs.get("remoteWebStartDeployDir"))
 		logger.warn("Given arguments:")
-		args.foreach{
-			a => logger.warn(a)
+		args.foreach {
+			logger.warn(_)
 		}
 
 		// check given arguments
@@ -221,14 +109,14 @@ object Deployer extends Actor with Utils {
 						copyAndInstallMacApp
 						// TODO: implement Mac Application deploy support
 					}
-					case "windows-app"  => {
-					    logger.warn("Requested to perform Windows Application deploy")
+					case "windows-app" => {
+						logger.warn("Requested to perform Windows Application deploy")
 						basicOnly_? = false
 						logger.error("Not yet implemented!")
 						// TODO: implement deployment of windows application
 					}
-					case "linux-app"  => {
-					    logger.warn("Requested to perform Linux Application deploy")
+					case "linux-app" => {
+						logger.warn("Requested to perform Linux Application deploy")
 						basicOnly_? = false
 						logger.error("Not yet implemented!")
 						// TODO: implement deployment of linux application
@@ -261,6 +149,124 @@ object Deployer extends Actor with Utils {
 				}
 			}
 		}
+
+		def getFilesFromMavenRepositoryAndSignThem = {
+			logger.warn("Searching for jars in Maven repository and signing them..")
+			var jar_names = List[String]()
+			if (basicOnly_?) {
+				jar_names ++= basic_jar_names
+				logger.warn("Selected only basic jars to deploy")
+			} else {
+				jar_names ++= basic_jar_names ++ dependency_jar_names
+				logger.warn("Selected basic and dependendant jars to deploy")
+			}
+			jar_names.foreach {
+				file =>
+						logger.info("*" + file + "*")
+						findFile(new File(pathToMaven2Repo), new P {
+							override
+							def accept(t: String): Boolean = {
+								val fileRegex = ".*" + file + "$"
+								val pattern = Pattern.compile(fileRegex)
+								val mat = pattern.matcher(t)
+								if (mat.find) {
+									if (!macAppDeploy)
+										signJar(t)
+									else { // don't sign jars when it's mac-app deploy
+										logger.warn("Copying jar: " + t.split("/").last)
+										FileUtils.copyFileToDirectory(new File(t), new File(deployTmpDir)) // copy files to temporary dir
+									}
+									return true
+								}
+								return false
+							}
+						}, filesToBeDeployed)
+			}
+		}
+
+
+		def signJar(fileToBeSigned: String) = {
+			new File(deployTmpDir).mkdir // make temporary place for jars before signinig
+			logger.warn("Signing jar: " + fileToBeSigned.split("/").last)
+			FileUtils.copyFileToDirectory(new File(fileToBeSigned), new File(deployTmpDir)) // copy files to temporary dir
+			val signCommand = Array(
+				prefs.get("jarSignerExecutable"), "-storepass", prefs.get("jarSignerPassword"),
+				deployTmpDir + fileToBeSigned.split("/").last, prefs.get("jarSignerKeyName")
+				)
+			logger.debug(CommandExec.cmdExec(signCommand).trim)
+		}
+
+
+		def backupLocal = {
+			val backupDate = (new Date).toString.replaceAll(" |:", "_")
+			val backupDir = new File(codebaseLocalDir + "../OLD_DEPLOYS/OLD_LOCAL_DEPLOY_" + backupDate + "/")
+			val localDeployDir = new File(codebaseLocalDir)
+			backupDir.mkdir
+			localDeployDir.mkdir
+			logger.warn("Copying files from " + localDeployDir + " to " + backupDir)
+			FileUtils.copyDirectory(localDeployDir, backupDir)
+		}
+
+
+		def deployLocal = {
+			backupLocal
+			filesToBeDeployed.toArray foreach {
+				localFile => {
+					val comparator = new JarEntryComparator
+					comparator.load(deployTmpDir + localFile.toString.split("/").last, codebaseLocalDir + "lib/" + localFile.toString.split("/").last)
+					if (comparator.diff_?) {
+						logger.warn("File DIFFERENT: " + localFile.toString.split("/").last)
+						FileUtils.copyFileToDirectory(new File(deployTmpDir + localFile.toString.split("/").last), new File(codebaseLocalDir + "lib/"))
+					} else {
+						logger.warn("File IDENTICAL: " + localFile.toString.split("/").last)
+					}
+				}
+			}
+			logger.warn("Generating JNLP file")
+			var arguments = ""
+			for (i <- prefs.getl("webstartArgumentsJVM")) {
+				arguments += i + " "
+			}
+			val jnlp = new JNLPSkeleton(
+				prefs.get("jnlpMainClass"),
+				prefs.get("jnlpAppName"),
+				"file://" + codebaseLocalDir,
+				prefs.get("jnlpFileName"),
+				(prefs.getl("deployFilesBasic") ++ prefs.getl("deployFilesAdditionalDependencies")),
+				arguments,
+				prefs.get("jnlpVendor"),
+				"file://" + codebaseLocalDir,
+				prefs.get("jnlpIcon"),
+				prefs.get("jnlpDescription")
+				)
+			val tempJnlpFileName = "/tmp/" + prefs.get("jnlpFileName")
+			jnlp.saveJNLP(tempJnlpFileName)
+			logger.warn("Putting JNLP file to local deploy dir")
+			FileUtils.copyFileToDirectory(new File(tempJnlpFileName), new File(codebaseLocalDir))
+		}
+
+
+		def copyAndInstallMacApp = {
+			logger.warn("Copying jars to temporary mac app")
+			FileUtils.copyDirectory(new File(deployTmpDir), new File(codebaseLocalDir + "../Coviob.app/lib")) // XXX: hardcoded Coviob.app name
+			// XXX: application skeleton must be uploaded to ~/.codadris before deploy
+			logger.warn("Copying Application to /Applications")
+			FileUtils.copyDirectory(new File(codebaseLocalDir + "../Coviob.app"), new File("/Applications/Coviob.app"))
+			logger.warn("Making executable of app starting script..")
+			CommandExec.cmdExec(Array("chmod", "777", "/Applications/Coviob.app/Contents/MacOS/coviob2")) // XXX: why the fuck copying files will result changing permissions to files?!
+		}
+
+
+		def deployerHelp = logger.warn("\n\nDeployer quick help:\nValid params:\n" +
+				"\tlocal -> for basic local deploy\n" +
+				"\tlocal-full -> for full local deploy\n" +
+				"\twindows-app -> for Windows application deploy\n" +
+				"\tlinux-app -> for Linux (distro independent) application deploy\n" +
+				"\tmac-app -> for Mac OS X application deploy and install\n" +
+				"\tfull -> for full remote deploy\n" +
+				"\tTo run remote deploy, no arguments (defaults based on xml config).")
+
+		0 // main should return 0 
 	}
 
 }
