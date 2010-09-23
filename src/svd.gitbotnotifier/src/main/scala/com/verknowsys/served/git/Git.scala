9327b167f17351bb87662a9944d58660eae359e0
Tymon Tobolski <i@teamon.eu>
2010-09-23 18:42:02 +0200

    Added comments to Git wrapper Added GitTest file

--- src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/git/Git.scala
+++ src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/git/Git.scala
@@ -10,50 +10,75 @@ import java.util.Date
 
 // XXX: http://wiki.eclipse.org/JGit/User_Guide - last paragraph
 
-object Git {
-	def main(args: Array[String]): Unit = {
-		val repo = new Repository("/Users/teamon/code/verknowsys/ServeD")
-		println()
-		println("com.verknowsys.served.git")
-		
-		println("---> repo.authors")
-		println(repo.authors)
-		println()
-		
-		println("---> repo.history")
-		println(repo.history.toList.size)
-		
-		repo.history("0261cf4d9283d1fdd45c27518a16a0d18ed231cc", "07ac043347fb979b5374ec6fdd064bcc246f005b").foreach {  c => 
-			println(c.sha)
-			println(c.date)
-			println(c.author.nameAndEmail)
-			println(c.message)
-			println()
-		}
-	}
-}
-
-import Git._
-
 /**
- * Proxy classes for more scala-like syntax
+ * JGit RevCommit wrapper for more scala-like syntax
  *
  * @author teamon
  */
 class Commit(val origin: RevCommit) {
+	/**
+	 * Returns commit`s date
+	 * 
+	 * @author teamon
+	 */
 	def date = new Date(origin.getCommitTime)
+	
+	/**
+	 * Returns commit`s SHA1
+	 * 
+	 * @author teamon
+	 */
 	def sha = origin.getName
+	
+	/**
+	 * Returns commit`s message
+	 * 
+	 * @author teamon
+	 */
 	def message = origin.getFullMessage
+	
+	/**
+	 * Returns commit`s author
+	 * 
+	 * @author teamon
+	 */
 	def author = origin.getAuthorIdent
 }
 
+/**
+ * JGit PersonIdent wrapper for more scala-like syntax
+ *
+ * @author teamon
+ */
 class Author(val origin: PersonIdent){
+	/**
+	 * Returns authors`s name
+	 * 
+	 * @author teamon
+	 */
 	def name = origin.getName
+	
+	/**
+	 * Returns authors`s email
+	 * 
+	 * @author teamon
+	 */
 	def email = origin.getEmailAddress
+	
+	/**
+	 * Returns authors`s name and email formatted as "name [email]"
+	 * 
+	 * @author teamon
+	 */
 	def nameAndEmail = "%s [%s]".format(name, email)
 }
 
-class Repository(dir: String) {
+/**
+ * GitRepository class  wrapper for more scala-like syntax
+ *
+ * @author teamon
+ */
+class GitRepository(dir: String) {
 	lazy val gitRepo = new JgitRepository(new File(dir, ".git"))
 	lazy val git = new Git(gitRepo)
 
@@ -66,7 +91,7 @@ class Repository(dir: String) {
 
 
 	/**
-	 * Returns commit history
+	 * Returns commit history (all commits)
 	 *
 	 * @author teamon
 	 */	
@@ -76,10 +101,22 @@ class Repository(dir: String) {
 		rw
 	}
 	
+	/**
+	 * Returns commit history for specified range
+	 *	@example
+	 *	    repo.history("0261cf4d9283d1fdd45c27518a16a0d18ed231cc", "07ac043347fb979b5374ec6fdd064bcc246f005b").foreach { commit => 
+	 *      	println(commit.sha)
+	 *			println(commit.date)
+	 *			println(commit.author.nameAndEmail)
+	 *			println(commit.message)
+	 *		}
+	 *
+	 * @author teamon
+	 */
 	def history(from: AnyObjectId, to: AnyObjectId) = git.log.addRange(from, to).call
 	
 	/**
-	 * Returns commit history
+	 * Returns map of authors: (Author`s name and email -> number of commits)
 	 *
 	 * @author teamon
 	 */
--- src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/git/package.scala
+++ src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/git/package.scala
@@ -2,7 +2,12 @@ package com.verknowsys.served
 
 import org.eclipse.jgit.lib.{ObjectId, PersonIdent}
 import org.eclipse.jgit.revwalk.RevCommit
-	
+
+/**
+ * Package object holding implicit conversions from JGit objects to its wrappers
+ *
+ * @author teamon
+ */	
 package object git {
 	implicit def RevCommitToCommit(c: RevCommit) = new Commit(c)
 	implicit def PersonIdentToAuthor(c: PersonIdent) = new Author(c)
