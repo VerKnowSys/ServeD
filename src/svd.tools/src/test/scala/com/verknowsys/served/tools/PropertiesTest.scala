1249fe139a3e4bb72097b4475fb6425e7439e6f8
Tymon Tobolski <i@teamon.eu>
2010-09-23 15:44:27 +0200

    Added Properties tests

--- src/svd.tools/src/test/scala/com/verknowsys/served/tools/PropertiesTest.scala
+++ src/svd.tools/src/test/scala/com/verknowsys/served/tools/PropertiesTest.scala
@@ -1,9 +1,38 @@
 package com.verknowsys.served.tools
 
 import org.specs._
+import java.io._
 
 class PropertiesTest extends SpecificationWithJUnit {
-	"it should work" in {
-		true must be equalTo(true)
+	final val configFilename = "/tmp/config.properties"
+
+	"Properties object" should {
+		doBefore { setupConfigFile }
+		"read from file" in {
+			val props = new Properties(configFilename)
+			props("app.existing.config.one") must_== Some("foo bar baz")
+			props("app.existing.other.config.two") must_== Some("foo bar baz bar blah")
+			props("app.not.existing.something") must_== None
+		}
+
+		"update properties file" in {
+			val props = new Properties(configFilename)
+			props("app.new.property.foo") = "very new"
+
+			val new_props = new Properties(configFilename)
+			new_props("app.existing.config.one") must_== Some("foo bar baz")
+			new_props("app.existing.other.config.two") must_== Some("foo bar baz bar blah")
+			new_props("app.not.existing.something") must_== None
+			new_props("app.new.property.foo") must_== Some("very new")
+		}
+	}
+
+	private def setupConfigFile {
+		val content = "app.existing.config.one = foo bar baz\napp.existing.other.config.two = foo bar baz bar blah"
+
+		val file = new File(configFilename)
+		val fw = new FileWriter(file)
+		try { fw.write(content) }
+		finally { fw.close }
 	}
 }
