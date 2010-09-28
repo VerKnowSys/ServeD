f2bf5f894838524098dbd0f74453b1220a115016
Tymon Tobolski <i@teamon.eu>
2010-09-28 23:33:43 +0200

    Added package object with implicit conversions for utils pacakge

---
+++ src/svd.utils/src/main/scala/com/verknowsys/served/utils/package.scala
@@ -0,0 +1,17 @@
+package com.verknowsys.served
+
+import java.io.File
+
+/**
+ * Package object holding implicit conversions for utils packge
+ *
+ * @author teamon
+ */	
+package object utils {
+    /**
+     * Convert string into File object
+     *
+     * @author teamon
+     */
+	implicit def StringToFile(s: String) = new File(s)
+}
