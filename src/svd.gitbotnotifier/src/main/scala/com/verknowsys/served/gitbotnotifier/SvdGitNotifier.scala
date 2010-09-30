f182450863da40028f75a2166d1b9c9934b1c7cc
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2010-10-01 00:03:24 +0200

    Rewritten SvdGitNotifier, better than last time

--- src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/gitbotnotifier/SvdGitNotifier.scala
+++ src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/gitbotnotifier/SvdGitNotifier.scala
@@ -3,8 +3,8 @@
 
 package com.verknowsys.served.gitbotnotifier
 
-import com.verknowsys.served.git.GitRepository
-import com.verknowsys.served.Config
+import com.verknowsys.served.git._
+import com.verknowsys.served._
 import com.verknowsys.served.utils._
 import com.verknowsys.served.utils.signals._
 
@@ -12,29 +12,121 @@ import com.verknowsys.served.utils.signals._
 // import java.io.OutputStreamWriter
 // import org.apache.log4j.{ConsoleAppender, Level, PatternLayout, Logger}
 import scala.actors._
+import org.jivesoftware.smack._
+import org.jivesoftware.smack.packet._
+import org.jivesoftware.smack.filter._
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._
 
 
+class SvdGitNotifier(repo: GitRepository) extends Actor with MessageListener with Utils {
 
 
-class SvdGitNotifier(repo: GitRepository) extends Actor with Utils {
+    def processMessage(chat: Chat, message: Message) {
+        logger.debug("Received message: " + message)
+    //     logger.debug("*** Received message: " + message + " (\"" + message.getBody + "\")")
+    //     if (message.getFrom.contains("verknowsys.com")) {   // XXX: hardcoded value
+    //         logger.debug("Message contains dmilith: " + message.getFrom)
+    //         message.getBody match {
+    //             case "last" =>
+    //                 chat.sendMessage("Requested last commit.\nNYI")
+    //             case "last5" =>
+    //                 chat.sendMessage("Requested last 5 commits.\nNYI")
+    //             case "last10" =>
+    //                 chat.sendMessage("Requested last 10 commits.\nNYI")
+    //             case "help" =>
+    //                 chat.sendMessage("No help for noobs")
+    //         }
+    //     }
+    }
+        
+    
     def act {
+
+        val config = new ConnectionConfiguration("verknowsys.com", 65222) // XXX: hardcode
+        val connection = new XMPPConnection(config)
+        val presence = new Presence(Presence.Type.available)
+        val login = "git-bot"
+        val password = "git-bot-666"
+        val resource = "served-bot-resource"
+        val chat = ListBuffer[Chat]()
+        
+        
+        def initConnection = {
+            debug {
+                XMPPConnection.DEBUG_ENABLED = true
+            }
+            config.setCompressionEnabled(true)
+            config.setSASLAuthenticationEnabled(false)
+            connection.connect
+            logger.debug("XMPP: login: " + login + ", pass:" + password + ", resource:" + resource)
+            try {
+                connection.login(login, password, resource)
+            } catch {
+                case x: Throwable =>
+                    logger.error("Error while connecting to XMPP server. Please check login / password.")
+                    logger.debug( x.printStackTrace )
+                    exit
+            }
+            val chatmanager = connection.getChatManager
+            logger.debug("Number of users: " + chat.length)
+            
+            ("dmilith@verknowsys.com" :: "i@teamon.eu" :: Nil).foreach { user =>
+                try {
+                    chat += chatmanager.createChat(user, this)
+                } catch {
+                    case x: Throwable =>
+                        logger.info("Error: " + x )
+                }
+            }
+            logger.debug("Number of users: " + chat.length)
+            presence.setStatus("ServeD Git Bot Notifier")
+            presence.setMode(Presence.Mode.dnd)
+            connection.sendPacket(presence)
+        }
+        
+
+        def closeConnection = connection.disconnect
+
+        
         val watchHEAD = FileEvents.watch(repo.dir + "/.git/logs") { name => name match {
-            case "HEAD" => logger.debug("HEAD changed")
+            case "HEAD" =>
+                logger.debug("HEAD changed in repo: " + repo.dir)
+                val commit = repo.history("HEAD", "HEAD~1").map{ e => e.message }.mkString(", ") // XXX: hardcoded
+                chat.foreach { element =>
+                    try {
+                        if (commit != null) {
+                            logger.debug("*** Trying to send messages, to User: " + element.getParticipant)
+                            element.sendMessage(commit)
+                            logger.debug("*** sent message: " + commit + " length: " + commit.length)
+                        }
+                    } catch {
+                        case e: Throwable =>
+                            logger.info("### Error " + e + "\nTrying to put commit onto list cause errors.")
+                            // DbAddCommit.writeCommitToDataBase(new Commit(commitSha))
+                    }
+                }
+                
             case _ =>
-        } }
+        }}
+        
         
         Actor.loop {
             receive {
                 case Init =>
-                    logger.debug("Git Notifier ready")
-
+                    initConnection
+                    logger.info("Git Notifier ready")
+                    
                 case Quit =>
                     logger.info("Quitting Git Notifier")
+                    closeConnection
                     exit
+                    
                 case _ =>
             }
         }
     }
+    
 }
 
 
