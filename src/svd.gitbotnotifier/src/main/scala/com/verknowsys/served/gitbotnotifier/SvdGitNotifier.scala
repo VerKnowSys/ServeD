3a20db5f6a1a038bf801e92979eb1fbe9959d368
Tymon Tobolski <i@teamon.eu>
2010-10-05 10:20:10 +0200

    Temporary removed watchHEAD

--- src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/gitbotnotifier/SvdGitNotifier.scala
+++ src/svd.gitbotnotifier/src/main/scala/com/verknowsys/served/gitbotnotifier/SvdGitNotifier.scala
@@ -105,31 +105,31 @@ class SvdGitNotifier(repo: GitRepository) extends Actor with MessageListener wit
             override def renamed(o_name: String, n_name: String) = logger.debug("RENAMED: %s -> %s".format(o_name, n_name))
         }
         
-        val watchHEAD = FileEvents.watch(repo.headPath){ fileName =>
-            if(fileName.contains(repo.headFile)){
-                logger.trace("HEAD changed in repo: %s".format(repo.dir))
-
-                repo.history(oldHEAD).foreach { commit =>
-                    logger.trace("Commit: " + commit)
-                    val message = "%s\n%s %s\n%s".format(commit.sha, new SimpleDateFormat("yyyy-MM-dd HH:mm").format(commit.date), commit.author.nameAndEmail, commit.message)
-
-                    chat.foreach { chatRecipient =>
-                        try {
-                            logger.debug("Trying to send messages, to User: " + chatRecipient.getParticipant)
-                            chatRecipient.sendMessage(message)
-                            logger.trace("Sent message: " + message + " length: " + message.length)
-                        } catch {
-                            case e: Throwable =>
-                                logger.info("### Error " + e + "\nTrying to put commit onto list cause errors.")
-                        }
-                    }
-                }
-
-                logger.trace("OldHead sha: %s".format(oldHEAD))
-                oldHEAD = repo.head
-                logger.trace("Assigned new sha: %s to oldHead".format(oldHEAD))
-            }
-        }
+        // val watchHEAD = FileEvents.watch(repo.headPath){ fileName =>
+        //     if(fileName.contains(repo.headFile)){
+        //         logger.trace("HEAD changed in repo: %s".format(repo.dir))
+        // 
+        //         repo.history(oldHEAD).foreach { commit =>
+        //             logger.trace("Commit: " + commit)
+        //             val message = "%s\n%s %s\n%s".format(commit.sha, new SimpleDateFormat("yyyy-MM-dd HH:mm").format(commit.date), commit.author.nameAndEmail, commit.message)
+        // 
+        //             chat.foreach { chatRecipient =>
+        //                 try {
+        //                     logger.debug("Trying to send messages, to User: " + chatRecipient.getParticipant)
+        //                     chatRecipient.sendMessage(message)
+        //                     logger.trace("Sent message: " + message + " length: " + message.length)
+        //                 } catch {
+        //                     case e: Throwable =>
+        //                         logger.info("### Error " + e + "\nTrying to put commit onto list cause errors.")
+        //                 }
+        //             }
+        //         }
+        // 
+        //         logger.trace("OldHead sha: %s".format(oldHEAD))
+        //         oldHEAD = repo.head
+        //         logger.trace("Assigned new sha: %s to oldHead".format(oldHEAD))
+        //     }
+        // }
 
         Actor.loop {
             receive {
