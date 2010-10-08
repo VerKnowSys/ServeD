b5c17f786907aa989f862a22720c633b9b462781
Tymon Tobolski <i@teamon.eu>
2010-10-08 13:02:07 +0200

    Added Notice result type

--- src/svd.api/src/main/scala/com/verknowsys/served/api/Messages.scala
+++ src/svd.api/src/main/scala/com/verknowsys/served/api/Messages.scala
@@ -1,6 +1,10 @@
 package com.verknowsys.served.api
 
+// ServeD -> Client messages
 case class Success(val msg: String)
+case class Notice(val msg: String)
 case class Error(val msg: String)
+
+// Client -> ServeD messages
 case class CreateGitRepository(val name: String)
 case class RemoveGitRepository(val name: String)
