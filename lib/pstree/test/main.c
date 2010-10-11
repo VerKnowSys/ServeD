650251b52ae75b7e30455aa7b6e6db13d0f74ca2
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2010-10-12 00:49:57 +0200

    Implemented basic test with dependencies. Throwing segfault currently

--- lib/pstree/test/main.c
+++ lib/pstree/test/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../pstree.c"
 
 
 int main(char* argv, char** args) {
 
     printf("%s", "dupa");
+    printf("%d", getPsTree());
+    
     return 0;
 }
