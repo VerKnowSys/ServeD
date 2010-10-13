4a8f586c0e6d60388b0bc9034d93c014faef4c8b
Tymon Tobolski <i@teamon.eu>
2010-10-13 22:29:30 +0200

    Refactored Account, moved to separate file

---
+++ src/svd.maintainer/src/main/scala/com/verknowsys/served/maintainer/Account.scala
@@ -0,0 +1,61 @@
+package com.verknowsys.served.maintainer
+
+import com.verknowsys.served.utils.Utils
+import com.verknowsys.served.utils.git.GitRepository
+import java.io.File
+import org.apache.commons.io.FileUtils
+
+case class Account(
+        val userName: String = "guest",
+        val pass: String = "x",
+        val uid: String = "1000",
+        val gid: String = "1000",
+        val information: String = "No information",
+        val homeDir: String = "/home/",
+        val shell: String = "/bin/bash"
+        ) extends Utils {
+            
+    def this(a: List[String]) = this(
+        userName = a(0),
+        pass = a(1),
+        uid = a(2),
+        gid = a(3),
+        information = a(4),
+        homeDir = a(5),
+        shell = a(6)
+    )
+    
+    
+    // def this(a: List[String]) = this(a(0), a(1), a(2), a(3), a(4), a(5), a(6))
+
+    def size = {
+        try {
+            val elementsSize = FileUtils.sizeOfDirectory(new File(homeDir))
+            logger.debug("getAccountSize of " + homeDir + " folder: " + elementsSize)
+            Some(elementsSize)
+        } catch {
+            case x: Exception =>
+                logger.error("Error: " + x)
+                None
+        }
+    }
+    
+    
+    // Git related stuff
+    
+    lazy val repositories = {
+        val list = new File(gitDir).list
+        if(list == null) List()
+        else list.toList
+    }
+    
+    protected lazy val gitDir = homeDir + "git/"
+    
+    def createRepository(name: String) = {
+        logger.trace("Creating new git repository %s for account %s".format(name, userName))
+        GitRepository.create(gitDir + name + ".git", bare = true)
+    }
+    
+    // TODO def setupGitWatchers
+
+}
--- src/svd.maintainer/src/main/scala/com/verknowsys/served/maintainer/SvdAccountManager.scala
+++ src/svd.maintainer/src/main/scala/com/verknowsys/served/maintainer/SvdAccountManager.scala
@@ -1,75 +1,15 @@
-// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
-// This Software is a close code project. You may not redistribute this code without permission of author.
-
 package com.verknowsys.served.maintainer
 
-/**
- * User: dmilith
- * Date: Dec 12, 2009
- * Time: 1:34:27 AM
- */
-
-import com.verknowsys.served._
-import com.verknowsys.served.utils._
-import com.verknowsys.served.utils.git._
-import com.verknowsys.served.utils.signals._
-import com.verknowsys.served.notifications._
-
-import scala.collection.mutable.ListBuffer
 import scala.actors.Actor
 import scala.io.Source
-import java.io._
-import java.nio.charset.Charset
-import org.apache.commons.io._
-import org.apache.log4j.{Level, Logger}
 
+import com.verknowsys.served.Config
+import com.verknowsys.served.utils._
+import com.verknowsys.served.utils.signals._
+import com.verknowsys.served.notifications.SvdGitNotifier
 
 case class GetUsers(val list: List[Account])
 case class Message(val value: String)
-
-
-case class Account(
-        val userName: String = "guest",
-        val pass: String = "x",
-        val uid: String = "1000",
-        val gid: String = "1000",
-        val information: String = "No information",
-        val homeDir: String = "/home/",
-        val shell: String = "/bin/bash"
-        ) extends Utils {
-            
-    def this(a: List[String]) = this(
-        userName = a(0),
-        pass = a(1),
-        uid = a(2),
-        gid = a(3),
-        information = a(4),
-        homeDir = a(5),
-        shell = a(6)
-    )
-    
-    lazy val repositories = loadRepositories
-    
-    // def this(a: List[String]) = this(a(0), a(1), a(2), a(3), a(4), a(5), a(6))
-
-    def size = {
-        try {
-            val elementsSize = FileUtils.sizeOfDirectory(new File(homeDir))
-            logger.debug("getAccountSize of " + homeDir + " folder: " + (elementsSize))
-            Some(elementsSize)
-        } catch {
-            case x: Exception =>
-                logger.error("Error: " + x)
-                None
-        }
-    }
-    
-    protected def loadRepositories = {
-        List(1,2)
-    }
-
-}
-
 
 object SvdAccountManager extends Actor with Utils {
     
@@ -139,8 +79,6 @@ object SvdAccountManager extends Actor with Utils {
 
 
 
-
-
     /**
      * @author dmilith
      *
@@ -153,5 +91,13 @@ object SvdAccountManager extends Actor with Utils {
             yield
                 new Account(line.split(":").toList)
     }
+    
+    def main(args: Array[String]): Unit = {
+        accounts.find (_.userName == "teamon") map { a =>
+            println(a.repositories.toList)
+            a.createRepository("xxx")
+        }
+    }
+
 
 }
