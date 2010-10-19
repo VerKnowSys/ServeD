e4610b29bb3fd96ddfb6e7b40521d3d581fa9ab8
Tymon Tobolski <i@teamon.eu>
2010-10-19 19:56:45 +0200

    Fixed compilation errors

--- src/svd.maintainer/src/main/scala/com/verknowsys/served/managers/AccountManager.scala
+++ src/svd.maintainer/src/main/scala/com/verknowsys/served/managers/AccountManager.scala
@@ -2,6 +2,8 @@ package com.verknowsys.served.managers
 
 import scala.actors.Actor
 import com.verknowsys.served.utils.Utils
+import com.verknowsys.served.utils.signals._
+import com.verknowsys.served.maintainer.Account
 
 /**
  * Account Manager - owner of all managers
@@ -14,9 +16,15 @@ class AccountManager(val account: Account) extends Actor with Utils {
     def act {
         loop {
             receive {
+                case Init =>
+                    logger.info("AccountManager ready for tasks")
+                    
+                case Quit =>
+                    logger.info("Quitting AccountManager...")
                 
+                case x: AnyRef =>
+                    logger.warn("Command not recognized. AccountManager will ignore it: " + x.toString)
             }
         }
     }
-
 }
