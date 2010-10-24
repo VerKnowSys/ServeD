4bb86f171697bb94357bc1e9f564892dcebbdee7
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2010-10-25 00:21:04 +0200

    More improvements to SystemManagerTest. Optimized tests. Fixed some stupid mistakes. Cleaned up

--- src/svd.systemmanager/src/test/scala/com/verknowsys/served/systemmanager/SystemManagerTest.scala
+++ src/svd.systemmanager/src/test/scala/com/verknowsys/served/systemmanager/SystemManagerTest.scala
@@ -54,42 +54,43 @@ class SvdSystemManagerTest extends SpecificationWithJUnit with UtilsCommon {
         
         "it must be able to get process list from system right away" in {
             
-            @specialized var index = 1000
+            @specialized var index, destIndex = 100
 
-            while (index > 0) {
-                
-                val a = processList(true, true).head
-                val b = processList(true, false).head
-                val c = processList(false, true).head
-                val d = processList(false, false).head
-                
-                for (i <- a :: b :: c :: d :: Nil) {
-                    i must notBeNull
-                    // i.pid.toInt must beGreaterThanOrEqualTo(0)
-                    i.processName must be matching("[a-zA-Z0-9]*")
+            val a = processList(true, true).head
+            val b = processList(true, false).head
+            val c = processList(false, true).head
+            val d = processList(false, false).head
+            val all = processList(true, true)
+            
+            for (i <- 0 to 9) {
+                index = destIndex
+                @specialized val start = (new java.util.Date).getTime
+                while (index > 0) {
+                    for (i <- a :: b :: c :: d :: Nil) {
+                        i must notBeNull
+                        // i.pid.toInt must beGreaterThanOrEqualTo(0)
+                        i.processName must be matching("[a-zA-Z0-9]*")
+                    }
+                    a.getClass.toString must be matching("SystemProcess")
+                    all.head.getClass.toString must be matching("SystemProcess")
+                    index -= 1
                 }
-
-                val all = processList(true, true)
-                println("Whole processList size: %d, element count: %d".format(sizeof(all), all.size))
-                println("Single value size: %d of class %s".format(sizeof(a), a.getClass))
-                a.getClass.toString must be matching("SystemProcess")
-                all.head.getClass.toString must be matching("SystemProcess")
-                
-                index -= 1
+                @specialized val stop = (new java.util.Date).getTime
+                println("TEST: %d. Time elapsed with %d elements: %dms".format(i + 1, destIndex, (stop - start)))
             }
+            
+            println("TEST: Whole processList size: %d, element count: %d".format(sizeof(all), all.size))
+            println("TEST: Single value size: %d of class %s".format(sizeof(a), a.getClass))
             
         }
         
         
         "it should be able to return amount of processes" in {
-            @specialized var outer = 100
+            @specialized var outer, o2 = 10
             @specialized val debug = false
-            
-            @specialized var index = 2500
-        
+            @specialized var index, i2 = 25
             @specialized val amountNoThreadsAndSorted = processCount(false, true)
             if (debug) println("TEST: amountNoThreadsAndSorted: %d".format(amountNoThreadsAndSorted))
-        
             @specialized val amountWithThreadsAndSorted = processCount(true, true)
             if (debug) println("TEST: amountWithThreadsAndSorted: %d".format(amountWithThreadsAndSorted))
         
@@ -98,30 +99,25 @@ class SvdSystemManagerTest extends SpecificationWithJUnit with UtilsCommon {
         
             @specialized val amountNoThreadsNoSorted = processCount(false, false)
             if (debug) println("\n\namountNoThreadsNoSorted: %d".format(amountNoThreadsNoSorted))
-        
             @specialized val amountWithThreadsNoSorted = processCount(true, false)
             if (debug) println("\n\namountWithThreadsNoSorted: %d".format(amountWithThreadsNoSorted))
         
+            @specialized val start = (new java.util.Date).getTime
             while (outer >0) {
-            
-                @specialized val start = (new java.util.Date).getTime
+                index = i2
                 while (index > 0) {
                     @specialized val current = processCount(true, true) // 2010-10-24 07:22:03 - dmilith - NOTE: should be heaviest, cause more processes + sorting
                     @specialized val current2 = processCount(true, false)
                     @specialized val current3 = processCount(false, false)
                     @specialized val current4 = processCount(false, true)
-                    amountNoThreadsAndSorted must beCloseTo(current4, 2) // 2010-10-24 07:24:31 - dmilith - NOTE: error delta +-10
+                    amountNoThreadsAndSorted must beCloseTo(current4, 2) // 2010-10-24 07:24:31 - dmilith - NOTE: error delta +-2
                     index -= 1
                 }
-                @specialized val stop = (new java.util.Date).getTime
-                println("TEST: Count size result time: " + (stop - start) + "ms.")
-                
-                if (outer % 10 == 0) {
-                    println("TEST: %s".format(processList(true, true)))
-                }
-                
                 outer -=1
             }
+            @specialized val stop = (new java.util.Date).getTime
+            println("TEST: Performed processCounts: %d. Result time: %d ms".format(i2*o2*4, (stop - start)))
+            
         }
 
 
