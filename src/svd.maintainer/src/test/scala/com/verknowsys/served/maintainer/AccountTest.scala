6d50c8c6ba930308b69384fc33efb4c917b99837
Tymon Tobolski <i@teamon.eu>
2010-10-25 20:14:28 +0200

    Added Account test

--- src/svd.maintainer/src/test/scala/com/verknowsys/served/maintainer/AccountTest.scala
+++ src/svd.maintainer/src/test/scala/com/verknowsys/served/maintainer/AccountTest.scala
@@ -1,71 +1,23 @@
+package com.verknowsys.served.maintainer
+
+import com.verknowsys.served._
+import org.specs._
 
 
+class AccountTest extends SpecificationWithJUnit {
+    "Account" should {
+        "correctly process params list" in {
+            val data = "teamon" :: "pass" :: "myUid" :: "myGid" :: "info" :: "/path/to/home" :: "/path/to/shell" :: Nil
+            val account = Account(data)
+        
+            account.userName must beEqual("teamon")
+            account.pass must beEqual("pass")
+            account.uid must beEqual("myUid")
+            account.gid must beEqual("myGid")
+            account.information must beEqual("info")
+            account.homeDir must beEqual("/path/to/home")
+            account.shell must beEqual("/path/to/shell")        
+        }
+    }
 
-
-
-
-
-// 2010-10-23 23:27:24 - dmilith - HACK: FIXME: XXX: NOTE: commented by me cause it fails and includes ugly hardcoded paths (non existant on other machines except Yours f.e.). Please use /tmp Luke and test it out Your code a bit more often ok? ;}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// package com.verknowsys.served.maintainer
-// 
-// import com.verknowsys.served._
-// import com.verknowsys.served.maintainer._
-// import org.specs._
-// import org.apache.commons.io.FileUtils
-// import java.io.File
-// 
-// 
-// class AccountTest extends SpecificationWithJUnit {
-//     "Account" should {
-//         // val account = Account(userName = "teamon", homeDir = "/tmp/svd/home/teamon/")
-//         // try { FileUtils.forceDelete(new File("/tmp/svd/home/teamon/")) } catch { case _ => }
-//         // FileUtils.forceMkdir(new File("/tmp/svd/home/teamon/"))
-// 
-//         // "Have empty repositories list" in {
-//         //             account.repositories must beEmpty
-//         //         }
-//         // 
-//         // "Create new git repository" in {
-//         //     account.createRepository("testrepo1")
-//         //     "/tmp/svd/home/teamon/git/testrepo1.git" must beAnExistingPath
-//         //     account.repositories.size must beEqual(1)
-//         //     
-//         //     account.createRepository("testrepo2")
-//         //     "/tmp/svd/home/teamon/git/testrepo2.git" must beAnExistingPath
-//         //     account.repositories.size must beEqual(2)
-//         //     
-//         //     // TODO: Catch exception
-//         //     // account.createRepository("testrepo2")
-//         //     // account.repositories.size must beEqual(2)
-//         // }
-//         
-//         
-//     }
-// 
-// }
+}
