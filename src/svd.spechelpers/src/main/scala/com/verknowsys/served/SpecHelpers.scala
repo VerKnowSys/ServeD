757ebeee9604877898b2b1527a9ea07fa0d7d460
Tymon Tobolski <i@teamon.eu>
2010-12-08 13:27:34 +0100

    Created svd.spechelpers module

---
+++ src/svd.spechelpers/src/main/scala/com/verknowsys/served/SpecHelpers.scala
@@ -0,0 +1,54 @@
+package com.verknowsys.served
+
+import org.apache.commons.io.FileUtils
+import java.io.File
+import scala.actors.Actor
+import scala.collection.mutable.ListBuffer
+
+object SpecHelpers {
+	implicit def StringToFile(s: String) = new File(s)
+    implicit def ItemToSeq[T](a: T) = a :: Nil
+    
+    def touch(file: File) = FileUtils.touch(file)
+    
+    def readFile(path: String) = FileUtils.readFileToString(path)
+
+    def writeFile(path: String, data: String) = FileUtils.writeStringToFile(path, data)
+    
+    def waitWhileRunning(actors: Seq[Actor]*) {
+        actors.flatten foreach { a =>
+            while(a.getState != Actor.State.Blocked && a.getState != Actor.State.Suspended) { 
+                if(a.getState != Actor.State.Terminated) println("[WARNING !!!] Actor Terminated - was supposed to be running - " + a)
+                waitFor(50)
+            }
+        }
+    }
+    
+    def waitForDeath(actors: Seq[Actor]*) {
+        actors.flatten foreach { a =>
+            while(a.getState != Actor.State.Terminated) { 
+                waitFor(50)
+            }
+        }
+    }
+    
+    def waitFor(time: Int) = Thread.sleep(time)
+    
+    class Counter extends Actor {
+        case object Stop
+        
+        val data = new ListBuffer[Any]
+        start
+        
+        def act {
+            loop {
+                receive {
+                    case Stop => exit
+                    case x: Any => data += x
+                }
+            }
+        }
+        
+        def stop { this ! Stop }
+    }
+}
