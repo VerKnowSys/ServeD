5678db0b530c8c8a11bde0e250392530569b2e20
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-01-09 17:31:14 +0100

    Extended NSP definition

--- svd.systemmanager/src/main/scala/com/verknowsys/systemmanager/served/NativeSystemProcess.scala
+++ svd.systemmanager/src/main/scala/com/verknowsys/systemmanager/served/NativeSystemProcess.scala
@@ -1,7 +1,6 @@
 package com.verknowsys.served.systemmanager
 
 import org.hyperic.sigar._
-import com.sun.jna.{Native, Library}
 import scala.collection.JavaConversions._
 
 /**
@@ -18,11 +17,11 @@ class NativeSystemProcess(val pid: Long) {
     
     
     val name: String = stat.getName
+    val user: String = core.getProcCredName(pid).getUser
     val ppid: Long = stat.getPpid
     val thr: Long = stat.getThreads
     val prio: Long = stat.getPriority
     val nice: Long = stat.getNice
-    
     val params: Array[String] = core.getProcArgs(pid)
     
     val timeStart: Long = cpu.getStartTime
@@ -32,11 +31,11 @@ class NativeSystemProcess(val pid: Long) {
     
     // val env = core.getProcEnv(pid).toMap
     
-    val residentMem: Long = mem.getResident
-    val sharedMem: Long = mem.getShare
-
+    val rss: Long = mem.getResident
+    val shr: Long = mem.getShare
+    
     val openFiles: Long = -1
     
-    override def toString = "N: %s, RES: %d, SHR: %d PID: %d, PPID: %d, THR: %d, PRIO: %d, NI: %d, PARAMS: [%s], TIME_START: %d, TIME_KERNEL: %d, TIME_TOTAL: %d, TIME_USER: %d, ENV: [%s], OPEN_FILES: %d\n\n".format(name, residentMem, sharedMem, pid, ppid, thr, prio, nice, params.mkString(" "), timeStart, timeKernel, timeTotal, timeUser, Some(null) getOrElse Nil.mkString("\n"), openFiles)
+    override def toString = "NAME:[%s], RES:[%d], SHR:[%d], PID:[%d], PPID:[%d], THREADS:[%d], PRIO:[%d], NICE:[%d], PARAMS:[%s], TIME_START:[%d], TIME_KERNEL:[%d], TIME_TOTAL:[%d], TIME_USER:[%d], OPEN_FILES:[%d]\n\n".format(name, rss, shr, pid, ppid, thr, prio, nice, params.mkString(" "), timeStart, timeKernel, timeTotal, timeUser, openFiles)
     
 }
