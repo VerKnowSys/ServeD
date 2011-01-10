57d0faba39c5eb0ed9ba05523491312aa27091b7
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-01-10 15:31:23 +0100

    Commented out more of old files

--- svd.utils/src/main/scala/OLD/jar/JarAccess.scala
+++ svd.utils/src/main/scala/OLD/jar/JarAccess.scala
@@ -1,34 +1,34 @@
-// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
-// This Software is a close code project. You may not redistribute this code without permission of author.
-
-package com.verknowsys.served.utils.jar
-
-
-import java.io.{InputStreamReader, BufferedReader}
-
-/**
- * User: dmilith
- * Date: Oct 3, 2009
- * Time: 7:52:04 PM
- */
-
-object JarAccess {
-    def readLineFromJARFile(filename: String) = {
-        lazy val is = getClass.getResourceAsStream(filename)
-        lazy val isr = new InputStreamReader(is)
-        lazy val br = new BufferedReader(isr)
-        lazy val sb = new StringBuffer
-        var line = ""
-        try {
-            sb.append(br.readLine) // only one - first line is interesting
-        } catch {
-            case x: Exception =>
-        }
-        br.close
-        isr.close
-        is.close
-        sb.toString
-    }
-
-
-}
+// // © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+// // This Software is a close code project. You may not redistribute this code without permission of author.
+// 
+// package com.verknowsys.served.utils.jar
+// 
+// 
+// import java.io.{InputStreamReader, BufferedReader}
+// 
+// /**
+//  * User: dmilith
+//  * Date: Oct 3, 2009
+//  * Time: 7:52:04 PM
+//  */
+// 
+// object JarAccess {
+//     def readLineFromJARFile(filename: String) = {
+//         lazy val is = getClass.getResourceAsStream(filename)
+//         lazy val isr = new InputStreamReader(is)
+//         lazy val br = new BufferedReader(isr)
+//         lazy val sb = new StringBuffer
+//         var line = ""
+//         try {
+//             sb.append(br.readLine) // only one - first line is interesting
+//         } catch {
+//             case x: Exception =>
+//         }
+//         br.close
+//         isr.close
+//         is.close
+//         sb.toString
+//     }
+// 
+// 
+// }
--- svd.utils/src/main/scala/OLD/skeletons/JNLPSkeleton.scala
+++ svd.utils/src/main/scala/OLD/skeletons/JNLPSkeleton.scala
@@ -1,95 +1,95 @@
-// © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
-// This Software is a close code project. You may not redistribute this code without permission of author.
-
-package com.verknowsys.served.utils.skeletons
-
-
-import xml.dtd.DocType
-import xml.XML
-
-/**
- * User: dmilith
- * Date: Jun 27, 2009
- * Time: 11:47:54 PM
- */
-
-class JNLPSkeleton(var mainClass: String,
-                   var title: String,
-                   var codebaseURL: String,
-                   var jnlpFileName: String,
-                   var jars: List[String],
-                   var paramsJVM: String,
-                   var vendor: String,
-                   var homepage: String,
-                   var icon: String,
-                   var description: String) {
-    def this(mainClass: String,
-             title: String,
-             codebaseURL: String,
-             jnlpFileName: String,
-             jars: List[String]) = this (mainClass, title, codebaseURL, jnlpFileName, jars, "", "", "", "", "")
-
-    def this(mainClass: String,
-             title: String,
-             codebaseURL: String,
-             jnlpFileName: String,
-             jars: List[String],
-             paramsJVM: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, "", "", "", "")
-
-    def this(mainClass: String,
-             title: String,
-             codebaseURL: String,
-             jnlpFileName: String,
-             jars: List[String],
-             paramsJVM: String,
-             vendor: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, vendor, "", "", "")
-
-    def this(mainClass: String,
-             title: String,
-             codebaseURL: String,
-             jnlpFileName: String,
-             jars: List[String],
-             paramsJVM: String,
-             vendor: String,
-             homepage: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, vendor, homepage, "", "")
-
-    def this(mainClass: String,
-             title: String,
-             codebaseURL: String,
-             jnlpFileName: String,
-             jars: List[String],
-             paramsJVM: String,
-             vendor: String,
-             homepage: String,
-             icon: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, vendor, homepage, icon, "")
-
-    def getJNLP =
-        <jnlp spec="1.0+" codebase={codebaseURL} href={jnlpFileName}>
-            <information>
-                <title>
-                    {title}
-                </title>
-                <vendor>
-                    {vendor}
-                </vendor>
-                <description>
-                    {description}
-                </description>
-                    <homepage href={homepage}/>
-                    <icon href={icon} kind="default"/>
-                    <offline-allowed/>
-            </information>
-            <security>
-                    <all-permissions/>
-            </security>
-            <resources>
-                    <j2se version="1.6+" java-vm-args={paramsJVM}/>{for (jar <- jars)
-            yield
-                    <jar href={"lib/" + jar} download="eager"/>}
-            </resources>
-            <application-desc main-class={mainClass}>
-            </application-desc>
-        </jnlp>;
-
-    def saveJNLP(file: String) = XML.save(file, getJNLP, "UTF-8", true, null)
-}
+// // © Copyright 2009 Daniel Dettlaff. ® All Rights Reserved.
+// // This Software is a close code project. You may not redistribute this code without permission of author.
+// 
+// package com.verknowsys.served.utils.skeletons
+// 
+// 
+// import xml.dtd.DocType
+// import xml.XML
+// 
+// /**
+//  * User: dmilith
+//  * Date: Jun 27, 2009
+//  * Time: 11:47:54 PM
+//  */
+// 
+// class JNLPSkeleton(var mainClass: String,
+//                    var title: String,
+//                    var codebaseURL: String,
+//                    var jnlpFileName: String,
+//                    var jars: List[String],
+//                    var paramsJVM: String,
+//                    var vendor: String,
+//                    var homepage: String,
+//                    var icon: String,
+//                    var description: String) {
+//     def this(mainClass: String,
+//              title: String,
+//              codebaseURL: String,
+//              jnlpFileName: String,
+//              jars: List[String]) = this (mainClass, title, codebaseURL, jnlpFileName, jars, "", "", "", "", "")
+// 
+//     def this(mainClass: String,
+//              title: String,
+//              codebaseURL: String,
+//              jnlpFileName: String,
+//              jars: List[String],
+//              paramsJVM: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, "", "", "", "")
+// 
+//     def this(mainClass: String,
+//              title: String,
+//              codebaseURL: String,
+//              jnlpFileName: String,
+//              jars: List[String],
+//              paramsJVM: String,
+//              vendor: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, vendor, "", "", "")
+// 
+//     def this(mainClass: String,
+//              title: String,
+//              codebaseURL: String,
+//              jnlpFileName: String,
+//              jars: List[String],
+//              paramsJVM: String,
+//              vendor: String,
+//              homepage: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, vendor, homepage, "", "")
+// 
+//     def this(mainClass: String,
+//              title: String,
+//              codebaseURL: String,
+//              jnlpFileName: String,
+//              jars: List[String],
+//              paramsJVM: String,
+//              vendor: String,
+//              homepage: String,
+//              icon: String) = this (mainClass, title, codebaseURL, jnlpFileName, jars, paramsJVM, vendor, homepage, icon, "")
+// 
+//     def getJNLP =
+//         <jnlp spec="1.0+" codebase={codebaseURL} href={jnlpFileName}>
+//             <information>
+//                 <title>
+//                     {title}
+//                 </title>
+//                 <vendor>
+//                     {vendor}
+//                 </vendor>
+//                 <description>
+//                     {description}
+//                 </description>
+//                     <homepage href={homepage}/>
+//                     <icon href={icon} kind="default"/>
+//                     <offline-allowed/>
+//             </information>
+//             <security>
+//                     <all-permissions/>
+//             </security>
+//             <resources>
+//                     <j2se version="1.6+" java-vm-args={paramsJVM}/>{for (jar <- jars)
+//             yield
+//                     <jar href={"lib/" + jar} download="eager"/>}
+//             </resources>
+//             <application-desc main-class={mainClass}>
+//             </application-desc>
+//         </jnlp>;
+// 
+//     def saveJNLP(file: String) = XML.save(file, getJNLP, "UTF-8", true, null)
+// }
