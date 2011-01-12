e8a03c13767cbb0a56496b0fe063359c0584edd0
Tymon Tobolski <i@teamon.eu>
2011-01-12 13:39:09 +0100

    Cleaned up Properties.scala

--- svd.utils/src/main/scala/com/verknowsys/served/utils/Properties.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/utils/Properties.scala
@@ -5,11 +5,32 @@ import scala.collection.JavaConversions._
 import java.io.{FileInputStream, FileOutputStream}
 import java.util.{Properties => JProperties}
 
+/** 
+ * Property converter interface
+ *   
+ * @author teamon
+ */
 abstract class PropertyConverter[T, Property] {
+    /** 
+     * Convert string into T-type
+     *   
+     * @author teamon
+     */
     def apply(s: String): Option[T]
+    
+    /** 
+     * Convert T-type into String
+     *   
+     * @author teamon
+     */
     def toString(a: T): String
 }
 
+/** 
+ * Object holding implicit converters
+ *   
+ * @author teamon
+ */
 object Property {
     implicit object StringPropertyConverter extends PropertyConverter[String, Property] {
         def apply(s: String) = Some(s)
@@ -32,6 +53,11 @@ object Property {
     }
 }
 
+/** 
+ * Single property representation
+ *   
+ * @author teamon
+ */
 class Property(parent: Properties, key: String){
     lazy val value = parent.data.flatMap(_ get key)
     
@@ -43,16 +69,17 @@ class Property(parent: Properties, key: String){
 
 
 /**
- * 	Class for handling Java Properties
+ * Class for handling Java Properties
+ *  
  * @example
  * val props = new Properties("config.properties")
- * 		props("foo.bar.baz") getOrElse "default"
- * 		props.int("foo.bar.baz") getOrElse 0
- * 		props.double("foo.bar.baz") getOrElse 1.0
- * 		props.bool("foo.bar.baz") getOrElse true
- * 		props("foo.bar.baz") = "new value"
- * 		props("foo.bar.baz") = 409
- * 		props("foo.bar.baz") = true
+ *      props("foo.bar.baz") or "default"
+ *      props("foo.bar.baz") or 0
+ *      props("foo.bar.baz") or 1.0
+ *      props("foo.bar.baz") or true
+ *      props("foo.bar.baz") = "new value"
+ *      props("foo.bar.baz") = 409
+ *      props("foo.bar.baz") = true
  *
  * @author teamon
  */
@@ -60,140 +87,24 @@ class Properties(filename: String) extends Utils {
     lazy val data = load
 
     /**
-     * 	Get value as Option[String]
+     *  Get value as Property object
      *
      * @author teamon
      */
-    // def apply(key: String): Option[String] = {
-    //     val value = data.flatMap(_ get key)
-    //     logger.trace("Setting props(%s) with value %s".format(key,value))
-    //     value
-    // }
-    
-
-    // == New API
-    
     def apply(key: String) = new Property(this, key)
     
+    /** 
+     * Update valueonverters
+     *   
+     * @author teamon
+     */
     def update[T](key: String, value: T)(implicit conv: PropertyConverter[T, Property]){
         data.foreach(_(key) = conv.toString(value))
         save
     }
     
-    // == end
-    
     /**
-     * 	Get value as String
-     *  
-     *   If key doesnt exist it saves the default value
-     *  
-     * @author teamon
-     */
-    // def apply(key: String, default: String): String = apply(key) getOrElse {
-    //     update(key, default)
-    //     default    
-    // }
-
-    /**
-     * 	Get value as Option[Int]
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def int(key: String): Option[Int] = apply(key).flatMap { s => try { Some(s.toInt) } catch { case _ => None } }
-    
-    /**
-     * Get value as Int
-     *  
-     *   If key doesnt exist it saves the default value
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def int(key: String, default: Int): Int = int(key) getOrElse { 
-        // update(key, default)
-        // default 
-    // }
-
-    /**
-     * 	Get value as Option[Double]
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def double(key: String): Option[Double] = apply(key).flatMap { s => try { Some(s.toDouble) } catch { case _ => None } }
-
-    /**
-     * Get value as Double
-     *  
-     *   If key doesnt exist it saves the default value
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def double(key: String, default: Double): Double = double(key) getOrElse { 
-    //     update(key, default)
-    //     default 
-    // }
-
-    /**
-     * 	Get value as Option[Boolean]
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def bool(key: String): Option[Boolean] = apply(key).flatMap { s => try { Some(s.toBoolean) } catch { case _ => None } }
-
-    /**
-     * Get value as Boolean
-     *  
-     *   If key doesnt exist it saves the default value
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def bool(key: String, default: Boolean): Boolean = bool(key) getOrElse { 
-    //     update(key, default)
-    //     default 
-    // }
-
-    /**
-     * 	Update value with String
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def update(key: String, value: String) {
-    //     data.foreach(_(key) = value)
-    //     save
-    // }
-
-    /**
-     * 	Update value with Int
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def update(key: String, value: Int) { update(key, value.toString) }
-
-    /**
-     * 	Update value with Double
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def update(key: String, value: Double) { update(key, value.toString) }
-
-    /**
-     * 	Update value with Boolean
-     *
-     * @author teamon
-     */
-    // @deprecated
-    // def update(key: String, value: Boolean) { update(key, value.toString) }
-
-    /**
-     * 	Loads properties file and returns Map
+     * Loads properties file and returns Map
      *
      * @author teamon
      */
@@ -210,15 +121,14 @@ class Properties(filename: String) extends Utils {
             
         } catch {
             case e: Exception =>
-                logger.error("Couldn`t load file %s".format(filename))
+                logger.error("Couldn/bin/bash: t: command not foundt save file %s, cause of exception: %s".format(filename, e))
                 None
         }
     }
 
     /**
-     * 	Saves data Map to file
+     * Saves data Map to file
      *
-     * @param fname filename
      * @author teamon
      */
     protected def save {
