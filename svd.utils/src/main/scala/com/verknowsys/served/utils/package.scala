caa76e34541bfeee55db4097f24e7d4e6760c6be
Tymon Tobolski <i@teamon.eu>
2011-01-13 19:25:18 +0100

    Added repl4str implicit for string replace using key-value map

--- svd.utils/src/main/scala/com/verknowsys/served/utils/package.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/utils/package.scala
@@ -14,5 +14,18 @@ package object utils {
      * @author teamon
      */
     implicit def StringToFile(s: String) = new File(s)
-
+    
+    /**
+     * Replace string usign key-value
+     * 
+     * {{{
+     *    scala> "foo %{bar} and %{baz}" % ("bar" -> "xxx", "baz" -> "blah")
+     *    res0: String = foo xxx and blah
+     * }}}
+     *
+     * @author teamon
+     */
+    implicit def repl4str(s: String) = new {
+        def %(pairs: (String, String)*) = (s /: pairs){ case (s, (k, v)) => s.replaceAll("%\\{" + k + "\\}", v) }
+    }
 }
