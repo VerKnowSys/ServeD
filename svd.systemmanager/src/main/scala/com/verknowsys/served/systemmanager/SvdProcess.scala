5da1d5bde92c8a208b8cb01cb323fd149f2f5774
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-01-20 03:12:24 +0100

    SvdProcess isn't actor anymore. Cleaned up code. new SvdProcess(…) automatically starts new thread and runs given command.
    Added commented requirements which should be also implemented soon.

--- svd.systemmanager/src/main/scala/com/verknowsys/served/systemmanager/SvdProcess.scala
+++ svd.systemmanager/src/main/scala/com/verknowsys/served/systemmanager/SvdProcess.scala
@@ -11,6 +11,7 @@ import scala.actors._
 import scala.actors.Actor._
 import scala.collection.JavaConversions._
 import java.io._
+import scala.io._
 import java.lang.reflect.{Field}
 
 /**
@@ -26,94 +27,85 @@ class SvdProcess(
     val workDir: String = "/tmp/",
     val outputRedirectDestination: String = "/tmp/served_redirXXX",
     val useShell: Boolean = true)
-        extends CommonActor with Monitored {
+        extends Logged {
     
+    // 2011-01-20 02:42:12 - dmilith - TODO: implement SvdProcess requirements
+    // require(commandIsntHarmful)
+    // require(workDirExists)
+    // require(usedShellValid)
+    // require(userListed)
     
-    var pid = -1 // 2011-01-18 14:03:05 - dmilith - XXX: temporary pid holder
-    var hasExited = false // 2011-01-18 14:03:20 - dmilith - XXX: temporary hasExited holder
-    var exitCode = -1 // 2011-01-18 14:11:34 - dmilith - XXX: temporary exitCode holder
-    var systemProcess: SystemProcess = null
+    debug("Spawning SystemProcess: (%s)".format(command))
     
-    start
+    Native.setProtected(true)
+
+    val pid = process
+    
+    trace("Process %s spawned.".format(command))
 
 
-    def act {
-        Native.setProtected(true)
-        loop {
-            receive {
-                case Run =>
-                    debug("Spawning SystemProcess(%s)".format(command))
-                    process
-                    this ! Quit
-                
-                case Quit =>
-                    trace("Process %s spawned.".format(command))
-                    exit
-                    
-                case x: Any =>
-                    error("Request for unsupported signal (%s) of SvdProcess: %s".format(x.toString, systemProcess))
-                    
-            }
+
+    // 2011-01-20 01:11:06 - dmilith - XXX: TODO: find out is this a most efficient way:
+    def alive = 
+        try {
+	        new SystemProcess(pid)
+	        true
+        } catch { 
+            case x: Any =>
+                trace("SvdProcess: '%s' has just thrown '%s' in alive()".format(command, x))
+                false
         }
-    }
+        
 
-    
-    
+    private
+
     /**
-      * Executes process and blocks thread
+      * Spawns new system process
+      *
       * @author dmilith
       *
-      * @result exitCode: Int
+      * @return spawned process pid
       *
       */
-    def process {
-        // 2011-01-10 23:33:11 - dmilith - TODO: implement params validation.
-        val cmdFormats = if (useShell) "%s -u %s -s %s > %s 2>&1" else "%s -u %s %s > %s 2>&1"
-        val cmd =  cmdFormats.format(Config.globalSudoExec, user, command, outputRedirectDestination).split(" ")
-        trace("CMD: %s".format(cmd.mkString(" ")))
-        val rt = Runtime.getRuntime
-        rt.traceMethodCalls(false)
-    
-        val env = Config.env
-        val proc = rt.exec(cmd, env)
+    def process: Long = {
+        var aPid: Long = 0L
+        val t = new Thread {
+            override def run = {
+                // 2011-01-10 23:33:11 - dmilith - TODO: implement params validation.
+                val cmdFormats = if (useShell) "%s -u %s -s %s > %s 2>&1" else "%s -u %s %s > %s 2>&1"
+                val cmd =  cmdFormats.format(Config.globalSudoExec, user, command, outputRedirectDestination).split(" ")
+                val rt = Runtime.getRuntime
+                val env = Config.env
+                val proc = rt.exec(cmd, env)
+                trace("CMD: %s".format(cmd.mkString(" ")))
+                rt.traceMethodCalls(false)
 
-        Thread.sleep(100) // 2011-01-18 15:44:11 - dmilith - XXX: wait for shell.. this sucks a bit right?
-        
-        // 2011-01-18 14:32:02 - dmilith - NOTE: HACK: XXX: matching for required fields twice, cause of unfavorable fields order (from reflection)
-        proc.getClass.getDeclaredFields.foreach{ f =>
-            f.setAccessible(true)
-            f.getName match {
-                case "pid" =>
-                    pid = f.get(proc).asInstanceOf[Int]
-                    systemProcess = new SystemProcess(pid)
-                    debug("Pid: %s (of %s)".format(pid, command))
+                proc.getClass.getDeclaredFields.foreach{ f =>
+                    f.setAccessible(true)
+                    f.getName match {
+                        case "pid" =>
+                            aPid = f.get(proc).asInstanceOf[Int]
+                            debug("Pid: %s (of %s)".format(aPid, command))
+
+                        case _ =>
+
+                    }
+                    trace(f.getName+"="+f.get(proc))
+                }
                 
-                case "hasExited" =>
-                    hasExited = f.get(proc).asInstanceOf[Boolean]
-                    if (hasExited) systemProcess = null
-                    debug("HasExited: %s (of %s)".format(hasExited, command))
-                
-                case _ =>
-            
+                try { 
+                    if (proc.exitValue > 0)
+                        throw new RuntimeException("'%s' exited abnormally with error code: '%s'. Output info: '%s'".format(command, proc.exitValue, Source.fromFile(outputRedirectDestination).mkString))
+                } catch {
+                  case e: IllegalThreadStateException =>
+                    trace("Process thrown: %s".format(e.getMessage))
+                }
             }
         }
-        // 2011-01-18 14:32:02 - dmilith - NOTE: HACK: XXX: matching for required fields twice, cause of unfavorable fields order (from reflection)
-        proc.getClass.getDeclaredFields.foreach{ f =>
-            f.setAccessible(true)
-            f.getName match {
-                case "exitcode" =>
-                    exitCode = if (hasExited) f.get(proc).asInstanceOf[Int] else -1 // throw -1 when process is still running
-                    debug("ExitCode: %s (of %s)".format(if (exitCode == -1) "still RUNNING process: %s".format(systemProcess) else exitCode, command))
-                    if (exitCode > 0)
-                        throw new Exception("'%s' exited abnormally.".format(command))
-                case _ =>
-            
-            }
-            trace(f.getName+"="+f.get(proc))
-        }
-        debug("Process: (%s) spawned in background".format(command))
-        // error("Process (%s) exception: %s".format(command, ex.getMessage))
+        t.start
+        t.join
+        aPid
     }
-            
-    
+
+
 }
