4140d27333fcf29f4b74c4c333590f504c02484c
Tymon Tobolski <i@teamon.eu>
2011-01-21 19:13:21 +0100

    Added AccountManager test

--- svd.maintainer/src/main/scala/com/verknowsys/served/managers/AccountManager.scala
+++ svd.maintainer/src/main/scala/com/verknowsys/served/managers/AccountManager.scala
@@ -4,45 +4,19 @@ import com.verknowsys.served.maintainer.Account
 import akka.actor.Actor
 import akka.util.Logging
 
+case object GetAccount
+
 /**
  * Account Manager - owner of all managers
  * 
  * @author teamon
  */
 class AccountManager(val account: Account) extends Actor with Logging {
-    // start
-    
-    // val gitManager = new GitManager(this)
-    
     log.trace("Starting AccountManager for account: " + account)
     
-    
     def receive = {
-        case x => println("got " + x)
+        case GetAccount => self reply account
+        case msg => log.warn("Message net recoginzed: %s", msg)
     }
-    
-    
-    // def act {
-    //     loop {
-    //         receive {
-    //             // case msg: Git.Base => sender ! (gitManager !! msg)
-    //             
-    //             case Init =>
-    //                 info("AccountManager ready")
-    //                 reply(Ready)
-    //                 
-    //             case Quit =>
-    //                 info("Quitting AccountManager(" + account.userName + ")")
-    //                 reply(Ready)
-    //                 exit
-    //             
-    //             case _ => messageNotRecognized(_)
-    //         }
-    //     }
-    // }
-    // 
-    // /**
-    //  * @author teamon
-    //  */
-    // override def toString = "AccountManager(" + account.userName + ")"
+
 }
