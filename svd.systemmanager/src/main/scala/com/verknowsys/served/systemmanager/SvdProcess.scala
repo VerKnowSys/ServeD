d1602fed27a74f5b2b6370fecb89078c105056b8
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-01-24 17:24:45 +0100

    Fixed dozen of SvdProcess issues. Added requirements for: commandNotEmpty, workDirExists, outputWritable. Parametrized defaults for SP in SvdConfig. Catch IllegalThreadStateException for SP and inform that SP thread has exited. Added ProcessException.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.systemmanager/src/main/scala/com/verknowsys/served/systemmanager/SvdProcess.scala
+++ svd.systemmanager/src/main/scala/com/verknowsys/served/systemmanager/SvdProcess.scala
@@ -12,6 +12,10 @@ import java.io._
 import scala.io._
 import java.lang.reflect.{Field}
 import akka.util.Logging
+import org.apache.commons.io.FileUtils
+
+
+class ProcessException(x: String) extends Exception(x)
 
 
 /**
@@ -20,22 +24,21 @@ import akka.util.Logging
   * @author dmilith
   */
 
-
 class SvdProcess(
-    val command: String = "",
-    val user: String = "nobody",
-    val workDir: String = "/tmp/",
-    val outputRedirectDestination: String = "/tmp/served_output_XXX", // 2011-01-22 17:39:06 - dmilith - NOTE: TODO: add date, and sha1
-    val useShell: Boolean = true)
+    val command: String,
+    val user: String = SvdConfig.noUser,
+    val workDir: String = SvdConfig.tmp,
+    val outputRedirectDestination: String = SvdConfig.nullDevice,
+    val useShell: Boolean = false)
         extends Logging {
     
+    log.trace("Command(%s) not empty? %s".format(command, commandNotEmpty))
     
-    // require(commandIsntHarmful, "Prohibited execution of possibily dangerous function: %s, as user: %s, with shell? %s, work dir: %s, output file: %s".format(command, user, useShell, workDir, outputRedirectDestination))
+    require(commandNotEmpty, "SvdProcess require non-empty command to execute!")
+    require(workDirExists, "SvdProcess working dir must exist! Given: %s".format(workDir))
+    require(outputWritable, "SvdProcess output file (%s) isn't writable!".format(outputRedirectDestination))
     
-    require(workDirExists, "Working dir must exist! Given: %s".format(workDir))
-
     // 2011-01-20 02:42:12 - dmilith - TODO: implement SvdProcess requirements
-    // require(commandExists, "Executed command seems to not exist. Execution terminated. Command: %s".format(command))
     // require(userListed)
     
     
@@ -45,31 +48,28 @@ class SvdProcess(
 
     lazy val pid = process
     
-    // val harmfulCommandDefinition = """.*\[rm\].*""".r // 2011-01-24 00:56:38 - dmilith - TODO: fill with harmful patterns
-    
     log.trace("Process %s spawned.".format(command))
 
     
         
-    // def commandExists =
-    //         command match {
-    //             case 
-    //         }
-
+    def commandNotEmpty =
+        (command != "") && (command != null)
+        
 
     def workDirExists =
         new File(workDir).exists
         
-
-    // def commandIsntHarmful =
-    //         command match {
-    //             case harmfulCommandDefinition() =>
-    //                 log.warn("Harmful command detected! Command: %s", command)
-    //                 false
-    //             case _ =>
-    //                 true
-    //         }
-
+    
+    def outputWritable = {
+        try { 
+            FileUtils.touch(outputRedirectDestination)
+        } catch {
+            case _ =>
+        }
+        new File(outputRedirectDestination).canWrite
+    }
+        
+        
 
     // 2011-01-20 01:11:06 - dmilith - TODO: find out is this a most efficient way:
     def alive = 
@@ -96,8 +96,8 @@ class SvdProcess(
       * @return spawned process pid
       *
       */
-    def process: Long = {
-        var aPid: Long = -1L
+    def process = {
+        var aPid = -1L
         val cmdFormats = if (useShell) "%s -u %s -s %s > %s 2>&1" else "%s -u %s %s > %s 2>&1"
         val cmd =  cmdFormats.format("sudo", user, command, outputRedirectDestination).split(" ")
         val rt = Runtime.getRuntime
@@ -110,7 +110,7 @@ class SvdProcess(
             f.setAccessible(true)
             f.getName match {
                 case "pid" =>
-                    aPid = f.get(proc).asInstanceOf[Int]
+                    aPid = f.get(proc).asInstanceOf[Int] // 2011-01-24 16:26:50 - dmilith - HACK! cause Long is real class, but ArrayIndexOutOfBoundException: 0 occurs when value is negative
                     log.debug("Pid: %s (of %s)".format(aPid, command))
 
                 case _ =>
@@ -119,16 +119,16 @@ class SvdProcess(
             log.trace(f.getName+"="+f.get(proc))
         }
         
-        try { 
-            if (proc.exitValue > 0)
-                throw new RuntimeException("SvdProcess: '%s' exited abnormally with error code: '%s'. Output info: '%s'".format(command, proc.exitValue, Source.fromFile(outputRedirectDestination).mkString))
-        } catch {
-            case e: IllegalThreadStateException =>
-                log.trace("Process thrown: %s".format(e.getMessage))
-
-            case e: FileNotFoundException =>
-                log.debug("Process '%s' output does not exists!".format(command))
-
+        try {
+        	if (proc.exitValue > 0)
+	            throw new ProcessException("SvdProcess: '%s' exited abnormally with error code: '%s'. Output info: '%s'".format(command, proc.exitValue,
+	                if (outputRedirectDestination != SvdConfig.nullDevice) Source.fromFile(outputRedirectDestination).mkString else "NONE"
+	            ))
+	    } catch { 
+	        case x: IllegalThreadStateException =>
+	            log.debug("SvdProcess thread exited. No exitValue given.")
+            case x: ProcessException =>
+                log.error("SvdProcess has exited with exception: %s".format(x))
         }
         
         aPid
