2079b16d8fee724e861bf0fc4e374a8457ceefa2
Tymon Tobolski <i@teamon.eu>
2011-01-25 14:19:49 +0100

    One more null check

--- svd.utils/src/main/scala/com/verknowsys/served/utils/SvdFileEventsManager.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/utils/SvdFileEventsManager.scala
@@ -110,7 +110,7 @@ class SvdFileEventsManager extends Actor with Logging {
         val event = new kevent
         val nev = clib.kevent(kq, null, 0, event, 1, null)
 
-        if(nev > 0 && event != null){
+        if(nev > 0 && event != null && event.ident != null){
             self ! SvdKqueueFileEvent(event.ident.intValue, event.fflags)
         } else if(nev == -1){
             throw new SvdKeventException // TODO: Catch this somehow
