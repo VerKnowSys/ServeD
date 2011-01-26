37b18714dda98dd02a468ce8ac215e8aa08e1038
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-01-26 12:26:51 +0100

    SvdProcess refactoring. Divided SvdProces to class and singleton (for external process requests). Implemented getProcessInfo(). No lock process spawning looks better now.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ svd.systemmanager/src/main/scala/com/verknowsys/served/systemmanager/native/SvdProcess.scala
@@ -0,0 +1,338 @@
+package com.verknowsys.served.systemmanager.native
+
+
+import com.verknowsys.served._
+import com.verknowsys.served.utils._
+import com.verknowsys.served.utils.signals._
+import com.verknowsys.served.utils.monitor._
+import com.verknowsys.served.systemmanager.native._
+
+import org.hyperic.sigar._
+import com.sun.jna.{Native, Library}
+import scala.collection.JavaConversions._
+import java.io._
+import scala.io._
+import java.lang.reflect.{Field}
+import akka.util.Logging
+import org.apache.commons.io.FileUtils
+import SvdPOSIX._
+
+
+class ProcessException(x: String) extends Exception(x)
+
+
+/**
+  * This class defines mechanism which system commands will be executed through (and hopefully monitored)
+  *
+  * @author dmilith
+  */
+
+class SvdProcess(
+    val command: String,
+    val user: String = SvdConfig.noUser,
+    val workDir: String = SvdConfig.tmp,
+    val outputRedirectDestination: String = SvdConfig.nullDevice,
+    val useShell: Boolean = false)
+        extends Logging {
+
+    
+    log.debug("Spawning SvdProcess: (%s)".format(command))
+
+    import SvdProcess._
+    import SvdPOSIX._
+    Native.setProtected(true)
+
+    // val pid = {
+    //         import CLibrary._
+    //         val clib = CLibrary.instance
+    //         if (clib.execve("/usr/local/bin/", "sudo -u %s %s".format(user, command).split(" "), Array("")) == 0)
+    //             clib.fork.asInstanceOf[Long]
+    //         else
+    //             -1
+    //     }
+    
+    val pid = {
+        var aPid = -1L
+        val cmdFormats = if (useShell) "%s -u %s -s %s > %s 2>&1" else "%s -u %s %s > %s 2>&1"
+        val cmd =  cmdFormats.format("sudo", user, command, outputRedirectDestination).split(" ")
+        val rt = Runtime.getRuntime
+        val env = SvdConfig.env
+        val proc = rt.exec(cmd, env)
+        log.trace("CMD: %s".format(cmd.mkString(" ")))
+        rt.traceMethodCalls(false)
+        proc.getClass.getDeclaredFields.foreach{ f =>
+            f.setAccessible(true)
+            f.getName match {
+                case "pid" =>
+                    aPid = f.get(proc).asInstanceOf[Int]
+                    log.debug("Pid: %s (of %s)".format(aPid, command))
+                case _ =>
+            }
+            log.trace(f.getName+"="+f.get(proc))
+        }
+        try {
+         if (proc.exitValue > 0)
+                 throw new ProcessException("SvdProcess: '%s' exited abnormally with error code: '%s'. Output info: '%s'".format(command, proc.exitValue,
+                     if (outputRedirectDestination != SvdConfig.nullDevice) Source.fromFile(outputRedirectDestination).mkString else "NONE"
+                 ))
+         } catch { 
+            case x: IllegalThreadStateException =>
+                log.debug("SvdProcess thread exited. No exitValue given.")
+            case x: ProcessException =>
+                log.error("%s".format(x))
+            // case x: SigarException =>
+            //                 log.debug("SigarException: %s".format(x))
+        }
+        aPid
+    }
+    
+    
+    def stat: ProcState = try {
+    	core.getProcState(pid)
+    } catch { 
+        case x: SigarException =>
+            log.error("Sigar has just throw: %s".format(x))
+            new ProcState
+    }
+    
+    
+    def cpu = try {
+    	core.getProcCpu(pid)
+    } catch { 
+        case x: SigarException =>
+            log.error("Sigar has just throw: %s".format(x))
+            new ProcCpu
+    }
+    
+    
+    def mem = try {
+    	core.getProcMem(pid)
+    } catch { 
+        case x: SigarException =>
+            log.error("Sigar has just throw: %s".format(x))
+            new ProcMem
+    }
+    
+    
+    def name = stat.getName
+    def runUser = core.getProcCredName(pid).getUser
+    def ppid = stat.getPpid
+    def thr = stat.getThreads
+    def prio = stat.getPriority
+    def nice = stat.getNice
+    def params: Array[String] = try {
+    	core.getProcArgs(pid)
+    } catch { 
+        case _ =>
+            Array("")
+    }
+    
+    def timeStart = cpu.getStartTime
+    def timeKernel = cpu.getSys
+    def timeTotal = cpu.getTotal
+    def timeUser = cpu.getUser
+    def rss = mem.getResident
+    def shr = mem.getShare
+
+
+    require(commandNotEmpty, "SvdProcess require non-empty command to execute!")
+    require(workDirExists, "SvdProcess working dir must exist! Given: %s".format(workDir))
+    require(outputWritable, "SvdProcess output file (%s) isn't writable!".format(outputRedirectDestination))
+    require(passACLs, "SvdProcess didn't pass ACL requirements! Failed process: %s".format(command))
+    require(pid > 0, "SvdProcess PID always should be > 0!")
+    // require(core != null, "SvdProcess Core cannot be null!")
+    // require(stat != null, "SvdProcess Stat cannot be null!")
+    // require(cpu != null, "SvdProcess Cpu cannot be null!")
+    // require(mem != null, "SvdProcess Mem cannot be null!")
+    
+    // 2011-01-20 02:42:12 - dmilith - TODO: implement SvdProcess requirements
+    // require(userListed)
+
+    
+
+    def passACLs =
+        true // 2011-01-25 20:56:10 - dmilith - TODO: implement ACL check
+    
+        
+    def commandNotEmpty =
+        (command != "") && (command != null)
+        
+
+    def workDirExists =
+        new File(workDir).exists
+        
+    
+    def outputWritable = {
+        try { 
+            FileUtils.touch(outputRedirectDestination)
+        } catch {
+            case _ =>
+        }
+        new File(outputRedirectDestination).canWrite
+    }
+        
+        
+
+    // 2011-01-20 01:11:06 - dmilith - TODO: find out is this a most efficient way:
+    def alive = 
+        try {
+             core.getProcState(pid)
+             true
+        } catch { 
+            case x: SigarException =>
+                log.trace("SvdProcess: %s alive() check thrown: %s".format(command, x))
+                false
+        }
+    
+    
+    /**
+      * Spawns new system process
+      *
+      * @author dmilith
+      *
+      * @return spawned process pid
+      *
+      */
+
+
+    /**
+      * Kills system process
+      *
+      * @author dmilith
+      *
+      * @return true if succeeded, false if failed
+      *
+      */
+    def kill(signal: SvdPOSIX.Value = SIGINT) =
+        SvdProcess.kill(pid, signal)
+
+
+    override def toString =
+           (
+           "PNAME:[%s] " +
+           "USER:[%s] " +
+           "RES:[%s] " +
+           "SHR:[%s] " +
+           "PID:[%s] " +
+           "PPID:[%s] " +
+           "THREADS:[%s] " +
+           "PRIO:[%s] " +
+           "NICE:[%s] " +
+           "COMMAND:[%s] " +
+           "TIME_START:[%s] " +
+           "TIME_KERNEL:[%s] " +
+           "TIME_TOTAL:[%s] " +
+           "TIME_USER:[%s] " +
+           "\n")
+               .format(name, user, rss, shr, pid, ppid, thr, prio, nice, params.mkString(" "), timeStart, timeKernel, timeTotal, timeUser)
+
+
+    log.trace("Process %s spawned.".format(command))
+    
+}
+
+
+/**
+ *  @author dmilith
+ *
+ *  Static access to kill process from outside of process
+ */
+object SvdProcess extends Logging {
+    
+    
+    val core = new Sigar
+    
+    
+    /**
+      * Kills system process with given pid and signal
+      *
+      * @author dmilith
+      *
+      * @return true if succeeded, false if failed
+      *
+      */
+    def kill(pid: Long, signal: SvdPOSIX.Value = SIGINT) = {
+        import CLibrary._
+        val clib = CLibrary.instance
+        if (clib.kill(pid, signal.id) == 0)
+            true
+        else
+            false
+    }
+    
+    
+    /**
+    *   @author dmilith
+    *   
+    *   Gets List of pids of whole system processes.
+    *   
+    *   Arguments: 
+    *       sort: Boolean. Default: false.
+    *           If true then it will return sorted alphabetically list of processes.
+    *
+    */
+    def processList(sort: Boolean = false) = {
+        val preList = core.getProcList.toList // 2010-10-24 01:09:51 - dmilith - NOTE: toList, cause JNA returns Java's "Array" here.
+        val sourceList = if (sort) preList.sortWith(_.toInt < _.toInt) else preList
+        log.debug("UnSORTED   : " + preList)
+        log.debug("SORTED     : " + preList.sortWith(_.toInt < _.toInt))
+        log.trace("sourceList : " + sourceList)
+        sourceList
+    }
+    
+    
+    /**
+    *   @author dmilith
+    *   
+    *   Returns System Process count.
+    *
+    *   Arguments:
+    *       sort: Boolean. Default: false
+    *
+    */
+    def processCount(sort: Boolean = false) = processList(sort).size
+    
+
+    /**
+     *  @author dmilith
+     *
+     *  Returns given pid process info as String
+     */
+    def getProcessInfo(apid: Long) = {
+        try {
+         val an = core.getProcState(apid)
+             (
+                "PNAME:[%s] " +
+                "USER:[%s] " +
+                "RES:[%s] " +
+                "SHR:[%s] " +
+                "PID:[%s] " +
+                "PPID:[%s] " +
+                "THREADS:[%s] " +
+                "PRIO:[%s] " +
+                "NICE:[%s] " +
+                "COMMAND:[%s] " +
+                "TIME_START:[%s] " +
+                "TIME_KERNEL:[%s] " +
+                "TIME_TOTAL:[%s] " +
+                "TIME_USER:[%s] " +
+                "\n")
+                    .format(an.getName, core.getProcCredName(apid).getUser, core.getProcMem(apid).getResident, core.getProcMem(apid).getShare, apid, an.getPpid, an.getThreads, an.getPriority, an.getNice, core.getProcArgs(apid).mkString(" "), core.getProcCpu(apid).getStartTime, core.getProcCpu(apid).getSys, core.getProcCpu(apid).getTotal, core.getProcCpu(apid).getUser)
+         
+        } catch { 
+            case _ =>
+                "NONE"
+        }
+    }
+    
+    
+    /**
+     *  @author dmilith
+     *
+     *  Returns current process pid
+     */
+    def getCurrentProcessPid =
+        core.getPid
+
+
+}
