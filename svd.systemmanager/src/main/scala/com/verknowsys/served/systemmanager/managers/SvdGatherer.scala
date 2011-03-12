7aa05c06ef19bebf2470379c7de85066ef4341d0
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-03-12 14:52:25 +0100

    Moved SvdGatherer to managers.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ svd.systemmanager/src/main/scala/com/verknowsys/served/systemmanager/managers/SvdGatherer.scala
@@ -0,0 +1,49 @@
+package com.verknowsys.served.systemmanager.managers
+
+import com.verknowsys.served.SvdConfig
+import com.verknowsys.served.utils._
+import com.verknowsys.served.systemmanager.native._
+import com.verknowsys.served.utils.signals._
+import com.verknowsys.served.api._
+
+import org.hyperic.sigar._
+import akka.actor.Actor
+import akka.actor.Actor.actorOf
+import akka.actor.Actor.registry
+import akka.util.Logging
+
+
+/**
+ *  @author dmilith
+ *
+ *   Gatherer will be spawned for each user account by SvdAccountManager.
+ *   The main goal for SvdGatherer is to gather user usage stats and write them to file.
+ *
+ */
+class SvdGatherer(account: SvdAccount) extends SvdManager(account) {
+    
+    
+    def userPostfix = account.userName + "/svd.gather"
+    
+    
+    def gatherFileLocation = 
+        if (SvdUtils.isBSD)
+            "/home/" + userPostfix
+        else
+            "/Users/" + userPostfix
+
+
+    log.trace("Starting SvdGatherer for account: %s. Account gath file: %s".format(account, gatherFileLocation))
+    
+    def receive = {
+        case Init =>
+            log.trace("gatherFileLocation: %s".format(gatherFileLocation))
+            log.debug("Initializing SvdGatherer for user %s".format(account.userName))
+            
+        case x =>
+            log.trace("gatherFileLocation: %s".format(gatherFileLocation))
+            log.debug("Received unknown signal in SvdGatherer: %s".format(x))
+    }
+ 
+    
+}
