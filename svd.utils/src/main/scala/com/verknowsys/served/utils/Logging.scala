5866fa78a84731628297f77d15edec69ecf15cf5
Tymon Tobolski <i@teamon.eu>
2011-05-12 21:59:05 +0200

    Cnsole logger implementation

--- svd.utils/src/main/scala/com/verknowsys/served/utils/Logging.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/utils/Logging.scala
@@ -1,26 +1,90 @@
 package com.verknowsys.served.utils
 
+import akka.actor.Actor
+import akka.event.EventHandler
+
 trait Logging {
-    @transient lazy val log = new Logger(this.getClass.getName)
+    @transient lazy val log = Logger(this.getClass.getName)
 }
 
 // object Logger {
 //     def apply(logger: String) = new Logger(logger)
 // }
 
-class Logger(klazz: String) {
-    def error(msg: String) { }
-    def error(msg: String, attrs: Any*): Unit = error(msg.format(attrs:_*))
+
+// Map(
+//     "com.verknowsys.served.utils" -> Warn,
+//     
+// )
+
+object Logger {
+    object Level extends Enumeration {
+        val Error, Warn, Info, Debug, Trace = Value
+    }
     
-    def warn(msg: String) { }
-    def warn(msg: String, attrs: Any*): Unit = warn(msg.format(attrs:_*))
+    def apply(className: String): Logger = new ConsoleLogger(className)
     
-    def info(msg: String) { }
-    def info(msg: String, attrs: Any*): Unit = info(msg.format(attrs:_*))
+    def levelFor(className: String): Level.Value = Level.Trace
+}
+
+// class LoggingManager extends Actor {
+//     def receive = {
+//         case LogLevel => self.reply
+//     }
+// }
+
+class ConsoleLogger(klazz: String) extends Logger(klazz){
+    protected[utils] def display(level: Logger.Level.Value, message: String, className: String) { 
+        println(level + " : (" + className + ") " + message)
+    }
+}
+
+abstract class Logger(klazz: String) {
+    import Logger.Level._
     
-    def debug(msg: String) { }
-    def debug(msg: String, attrs: Any*): Unit = debug(msg.format(attrs:_*))
+    def error(msg: => String) = log(Error, msg)
+    def error(msg: String, arg: Any, args: Any*): Unit = error(msg.format((arg :: args.toList):_*))
     
-    def trace(msg: String) { }
-    def trace(msg: String, attrs: Any*): Unit = trace(msg.format(attrs:_*))
+    def warn(msg: => String) = log(Warn, msg)
+    def warn(msg: String, arg: Any, args: Any*): Unit = warn(msg.format((arg :: args.toList):_*))
+    
+    def info(msg: => String) = log(Info, msg)
+    def info(msg: String, arg: Any, args: Any*): Unit = info(msg.format((arg :: args.toList):_*))
+    
+    def debug(msg: => String) = log(Debug, msg)
+    def debug(msg: String, arg: Any, args: Any*): Unit = debug(msg.format((arg :: args.toList):_*))
+    
+    def trace(msg: => String) = log(Trace, msg)
+    def trace(msg: String, arg: Any, args: Any*): Unit = trace(msg.format((arg :: args.toList):_*))
+    
+    protected[utils] def level = Logger.levelFor(klazz)
+    
+    protected[utils] def log(lvl: Logger.Level.Value, message: => String, className: String = klazz) = {
+        if(level >= lvl) display(lvl, message, className)
+    }
+
+    protected[utils] def display(level: Logger.Level.Value, message: String, className: String)
+}
+
+class LoggingEventHandler extends Actor with Logging {
+    import EventHandler._
+    
+    self.dispatcher = EventHandler.EventHandlerDispatcher
+    
+    def receive = {
+        case Error(cause, instance, message) => 
+            log.log(Logger.Level.Error, message.toString, instance.getClass.getName)
+        
+        case Warning(instance, message) =>  
+            log.log(Logger.Level.Warn, message.toString, instance.getClass.getName)
+        
+        case Info(instance, message) =>
+            log.log(Logger.Level.Info, message.toString, instance.getClass.getName)
+            
+        case Debug(instance, message) =>
+            log.log(Logger.Level.Debug, message.toString, instance.getClass.getName)
+            
+        case event => 
+            log.debug(event.toString)
+    }
 }
