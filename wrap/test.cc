775bd4ece6a7c0dc7e47f358aab62c223077c4a0
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-06-12 00:54:18 +0200

    Perform only 10 routines in wrapper test.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- wrap/test.cc
+++ wrap/test.cc
@@ -3,7 +3,7 @@
 /* example in case of standalone building (no dylib/so mode) */
 int main() {
 
-    for (int i; i < 100000; i++) {
+    for (int i; i < 10; i++) {
         cout << spawn(501, (char*)"/bin/cat /etc/passwd >> /var/tmp/500.log", (char*)"/var/tmp/OUT_TEST_LOG") << endl; // NOTE: this wont work cause uid 75 don't have access to /var/log/kernel.log
         cout << spawn(75, (char*)"/bin/sleep 50", (char*)"/dev/null") << endl;
         cout << spawn(75, (char*)"/bsdggdgsgsdgfdsgsdfdin/ls -la -m", (char*)"/var/tmp/OUT_TEST_LSLAM") << endl;
