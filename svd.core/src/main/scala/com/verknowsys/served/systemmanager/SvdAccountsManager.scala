d4d8422cb0a251ebac1ee0562611e9edc09ba56a
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-06-22 17:02:57 +0200

    Updated SvdAccountsManager. Implemented auto spawn of users with uids corresponding to /SystemUsers content. Using Logging. Disabled global file watcher on /etc/passwd. From now, all virtual users should be independent from each other.
    Also added hack to create default system user with uid "501"
    
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.core/src/main/scala/com/verknowsys/served/systemmanager/SvdAccountsManager.scala
+++ svd.core/src/main/scala/com/verknowsys/served/systemmanager/SvdAccountsManager.scala
@@ -11,13 +11,14 @@ import com.verknowsys.served.api._
 import akka.actor.{Actor, ActorRef}
 import akka.actor.Actor.{remote, actorOf, registry}
 import scala.io.Source
-
+import java.io.File
 
 
 case class GetAccountManager(username: String)
 
 
-class SvdAccountsManager extends Actor with SvdFileEventsReactor with SvdExceptionHandler {
+class SvdAccountsManager extends Actor with SvdFileEventsReactor with SvdExceptionHandler with Logging {
+    
     import events._
     
     log.info("SvdAccountsManager is loading")
@@ -25,21 +26,23 @@ class SvdAccountsManager extends Actor with SvdFileEventsReactor with SvdExcepti
     // case object ReloadUsers
     // case class CheckUser(val username: String)
     
-    protected val systemPasswdFilePath = SvdConfig.systemPasswdFile // NOTE: This must be copied into value to use in pattern matching
+    // protected val systemPasswdFilePath = SvdConfig.systemPasswdFile // NOTE: This must be copied into value to use in pattern matching
     
     // Safe map for fast access to AccountManagers
     protected var accountManagers: Option[Map[String, ActorRef]] = None
 
+
     def receive = {
         case Init =>
             log.debug("SvdAccountsManager received Init. Running default task..")
-            registerFileEventFor(SvdConfig.systemPasswdFile, Modified)
+            // registerFileEventFor(SvdConfig.systemHomeDir, Modified)
             respawnUsersActors
             self reply_? Success
         
-        case SvdFileEvent(systemPasswdFilePath, Modified) => 
-            log.trace("Passwd file modified")
-            respawnUsersActors
+        // 2011-06-22 01:34:12 - dmilith - PENDING: FIXME: TODO: fix file watcher issues
+        // case SvdFileEvent(systemPasswdFilePath, Modified) => 
+            // log.trace("Passwd file modified")
+            // respawnUsersActors
             
         case GetAccountManager(username) =>
             self reply accountManagers.flatMap(_.get(username))
@@ -63,7 +66,15 @@ class SvdAccountsManager extends Actor with SvdFileEventsReactor with SvdExcepti
         // }.toMap)
         // log.trace("Actor.registry size after: %d", registry.actors.size)
         
-        val map = Map(
+        val wrapper = SvdWrapLibrary.instance
+        userAccounts.foreach{
+            ua =>
+                log.warn("Sending spawn message for account: %s".format(ua))
+                wrapper.sendSpawnMessage(ua.uid.toString)
+                
+        }
+        
+        val map = Map( // 2011-06-22 16:49:20 - dmilith - XXX: FIXME: TODO: merge it into SvdAccount (gather automatically)
             "teamon" -> ("localhost", 8000),
             "dmilith" -> ("localhost", 8000)
         )
@@ -77,20 +88,29 @@ class SvdAccountsManager extends Actor with SvdFileEventsReactor with SvdExcepti
 
     
     /**
-     * Function to parse and convert passwd file entries to List[Account]
-     * @author teamon
+     * Function to read user accounts from filesystem
+     * @author dmilith
      */
-    protected def allAccounts = {
-        val rawData = Source.fromFile(SvdConfig.systemPasswdFile, SvdConfig.defaultEncoding).getLines.toList
-        for(SvdAccount(account) <- rawData) yield account
+    protected def userAccounts = {
+        val shd = new File(SvdConfig.systemHomeDir)
+        if (shd.exists) {
+            val systemHomeDir = shd.listFiles.toList
+            val accounts = systemHomeDir.map{
+                hd =>
+                    log.warn("HD: %s".format(hd))
+                    new SvdAccount(uid = hd.getPath.split("/").reverse.head.toInt)
+            }
+            log.debug("allAccounts(), systemHomeDir: %s".format(systemHomeDir))
+            accounts
+        } else {
+            new File(SvdConfig.systemHomeDir).mkdirs
+            log.trace("Created %s. No users in system!".format(SvdConfig.systemHomeDir))
+            
+            new File(SvdConfig.systemHomeDir / "501") // 2011-06-22 17:00:15 - dmilith - HACK: HARDCODE: XXX: FIXME: auto "add" default user
+            
+            Nil
+        }
     }
      
      
-    /**
-     * Returns only normal users' accounts
-     * @author teamon
-     */
-    protected def userAccounts = allAccounts.filter(_.isUser)
-
-    
 }
