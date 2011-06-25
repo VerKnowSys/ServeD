66d6270e3d1566754c48baf9c7c4534b777643e6
Tymon Tobolski <i@teamon.eu>
2011-06-26 01:28:18 +0200

    Refactored Git tests

--- svd.core/src/test/scala/com/verknowsys/served/git/GitTest.scala
+++ svd.core/src/test/scala/com/verknowsys/served/git/GitTest.scala
@@ -1,234 +1,223 @@
 package com.verknowsys.served.git
 
 import java.io.File
+import com.verknowsys.served.testing._
 import com.verknowsys.served.SvdSpecHelpers._
 import com.verknowsys.served.utils._
 import org.eclipse.jgit.api.errors.NoHeadException
-import org.specs._
+// import org.specs._
 
-class GitTest extends Specification {
+class GitTest extends DefaultTest {
+    val path = randomPath
+    mkdir(path)
 
-    "GitRepository object" should {
-        doBefore {
-            reloadTestDir
-        }
+    override def afterEach {
+        rmdir(path)
+    }
 
-        "create new normal repository" in {
-            val repo = Git.init(testPath("newrepo"))
-            repo.path must_== testPath("newrepo")
-            repo.name must_== "newrepo"
-            repo.isBare must beFalse
-            
-            val file = new File(testPath("newrepo"))
-            file.exists must beTrue
-            file.isDirectory must beTrue
-            
-            val dotgit = new File(testPath("newrepo/.git"))
-            dotgit.exists must beTrue
-            dotgit.isDirectory must beTrue
-        }
+    "GitRepository" should "create new normal repository" in {
+        val repo = Git.init(path / "newrepo")
+        repo.path should equal (path / "newrepo")
+        repo.name should equal ("newrepo")
+        repo should not be ('bare)
         
-        "create new bare repository" in {
-            val repo = Git.init(testPath("newbarerepo"), bare = true)
-            repo.name must_== "newbarerepo"
-            repo.isBare must beTrue
-            
-            val file = new File(testPath("newbarerepo.git"))
-            file.exists must beTrue
-            file.isDirectory must beTrue
-            
-            val dotgit = new File(testPath("newbarerepo.git/.git"))
-            dotgit.exists must beFalse
-        }
+        val file = new File(path / "newrepo")
+        file should (exist)
+        file should be ('directory)
         
-        "clone remote repository" in {
-            val source = Git.init(randomPath)
-            writeFile(source.path / "README", "Remote repo README")
-            source.add("README")
-            source.commit("initial for clone")
-            
-            val target = Git.clone(randomPath, source.path)
-            target.history must haveSize(1)
-            target.history.next.message must_== "initial for clone"
-        }
+        val dotgit = new File(path / "newrepo" / ".git")
+        dotgit should (exist)
+        dotgit should be ('directory)
+    }
         
-        "list repositories in directory" in {
-            val path = randomPath
-            Git.list(path) must haveSize(0)
-            
-            Git.init(path / "one")
-            Git.init(path / "two")
-            Git.init(path / "three")
-            
-            val list = Git.list(path)
-            list must haveSize(3)
-            list.map(_.name) must containAll("one" :: "two" :: "three" :: Nil)
-            
-        }
+    it should "create new bare repository" in {
+        val repo = Git.init(path / "newbarerepo", bare = true)
+        repo.name should equal ("newbarerepo")
+        repo should be ('bare)
+        
+        val file = new File(path / "newbarerepo.git")
+        file should (exist)
+        file should be ('directory)
+        
+        val dotgit = new File(path / "newbarerepo.git" / ".git")
+        dotgit should not (exist)
+    }
+        
+    it should "clone remote repository" in {
+        val source = Git.init(path)
+        writeFile(source.path / "README", "Remote repo README")
+        source.add("README")
+        source.commit("initial for clone")
+        
+        val target = Git.clone(randomPath, source.path)
+        target.history.size should equal (1)
+        target.history.next.message should equal ("initial for clone")
     }
     
-    "GitRepository commands" should {
-        doBefore {
-            reloadTestDir
-        }
+    it should "list repositories in directory" in {
+        Git.list(path) should have size (0)
         
-        "have null HEAD" in {
-            val repo = Git.init(randomPath)
-            repo.head.getObjectId must beNull
-        }
+        Git.init(path / "one")
+        Git.init(path / "two")
+        Git.init(path / "three")
         
-        "add new file and commit" in {
-            val repo = Git.init(randomPath)
-            writeFile(repo.path / "README", "Some readme text")
-            repo.add("README")
-            repo.commit("init")
-
-            repo.history must haveSize(1)
-            
-            val head = repo.head
-            val first = repo.history.next
-
-            head.getObjectId mustNot beNull
-            first.message must_== "init"
-
-            head.sha must_== first.sha
-            
-            repo.currentBranch must_== "master"
-        }
-        
-        "make few commits" in {
-            val repo = Git.init(randomPath)
-            writeFile(repo.path / "README", "Some readme text")
-            repo.add("README")
-            repo.commit("init")
-
-            (1 to 10) foreach { i =>
-                writeFile(repo.path / "README", "Changed to " + i)
-                repo.add("README")
-                repo.commit("changed " + i)
-            }
-
-
-            repo.history must haveSize(11)
-            
-            repo.history.take(3).map(_.message).toList must_== List("changed 10", "changed 9", "changed 8")
-            
-            val sha3 = repo.history.drop(7).next.sha
-            val sha5 = repo.history.drop(5).next.sha
-            
-            repo.history(sha3) must haveSize(7)
-            repo.history(sha5) must haveSize(5)
-        }
-        
-        "remote" in {
-            val repo = Git.init(randomPath)
-            repo.remotes must haveSize(0)
-
-            repo.addRemote("origin", "/path/to/remote.git")
-            repo.remotes must haveSize(1)
-
-            repo.addRemote("github", "githuburl.git")
-            repo.remotes must haveSize(2)
-
-            repo.remotes.map(_.name) must containAll("origin" :: "github" :: Nil)
-            
-            val sameRepo = new GitRepository(repo.path)
-            sameRepo.remotes must haveSize(2)
-        }
-        
-        "push" in {
-            val repo = Git.init(randomPath)
-            writeFile(repo.path / "README", "Some readme text")
-            repo.add("README")
-            repo.commit("init")
-            
-            val remote = Git.init(randomPath)
-            remote.history must throwA[NoHeadException]
-            
-            repo.addRemote("origin", remote.path)
-            repo.push()
-            
-            remote.history must haveSize(1)
-        }
-        
-        "pull" in {
-            val source = Git.init(randomPath)
-            writeFile(source.path / "README", "Remote repo README")
-            source.add("README")
-            source.commit("initial")
-
-
-            val target = Git.init(randomPath)
-            target.addRemote("origin", source.path)
-            target.pull
-
-            target.history must haveSize(1)
-        }
-        
-        "branch" in {
-            val repo = Git.init(randomPath)
-            writeFile(repo.path / "README", "Some readme text")
-            repo.add("README")
-            repo.commit("init")
-            
-            repo.branchList must haveSize(1)
-                        
-            repo.branchList.map(_.name.split("/").last) must contain("master")
-            
-            repo.branch("feature")
-            
-            repo.branchList must haveSize(2)
-            repo.branchList.map(_.name.split("/").last) must containAll("master" :: "feature" :: Nil)
-        }
-        
-        "checkout" in {
-            val repo = Git.init(randomPath)
-            writeFile(repo.path / "README", "Some readme text")
-            repo.add("README")
-            repo.commit("init")
-            
-            repo.currentBranch must_== "master"
-            
-            repo.branch("feature")
-            repo.checkout("feature")
-            
-            writeFile(repo.path / "README", "Some readme text added only in feature branch")
-            repo.add("README")
-            repo.commit("changed README")
-            
-            repo.history must haveSize(2)
-            
-            repo.checkout("master")
-            repo.history must haveSize(1)
-        }
-        
-        "single commit" in {
-            val repo = Git.init(randomPath)
-            writeFile(repo.path / "README", "Some readme text")
-            repo.add("README")
-            
-            val me = new Author("T. Tobolski", "teamon@example.com")
-            val him = new Author("D. Dettlaff", "dmilith@example.com")
-            repo.commit("init", author = me, committer = him)
-            
-            val commit = repo.history.next
-            commit.message must_== "init"
-            commit.author.name must_== "T. Tobolski"
-            commit.author.email must_== "teamon@example.com"
-            commit.committer.name must_== "D. Dettlaff"
-            commit.committer.email must_== "dmilith@example.com"
-            
-            commit.sha must_== repo.head.sha
-        }
-
+        val list = Git.list(path).map(_.name)
+        list should have size(3)
+        list should contain ("one")
+        list should contain ("two")
+        list should contain ("three")
     }
     
-    "Author" should {
-        "init" in {
-            val author = new Author("T. Tobolski", "teamon@example.com")
-            author.name must_== "T. Tobolski"
-            author.email must_== "teamon@example.com"
+    "GitRepository commands" should "have null HEAD" in {
+        val repo = Git.init(randomPath)
+        repo.head.getObjectId should be (null)
+    }
+    
+    it should "add new file and commit" in {
+        val repo = Git.init(path)
+        writeFile(repo.path / "README", "Some readme text")
+        repo.add("README")
+        repo.commit("init")
+        
+        repo.history.size should equal (1)
+        
+        val head = repo.head
+        val first = repo.history.next
+        
+        head.getObjectId should not be (null)
+        first.message should equal ("init")
+        
+        head.sha should equal (first.sha)
+        
+        repo.currentBranch should equal ("master")
+    }
+    
+    it should "make few commits" in {
+        val repo = Git.init(path)
+        writeFile(repo.path / "README", "Some readme text")
+        repo.add("README")
+        repo.commit("init")
+    
+        (1 to 10) foreach { i =>
+            writeFile(repo.path / "README", "Changed to " + i)
+            repo.add("README")
+            repo.commit("changed " + i)
         }
+        
+        repo.history.size should equal (11)
+        repo.history.take(3).map(_.message).toList should equal (List("changed 10", "changed 9", "changed 8"))
+        
+        val sha3 = repo.history.drop(7).next.sha
+        val sha5 = repo.history.drop(5).next.sha
+        
+        repo.history(sha3).size should equal (7)
+        repo.history(sha5).size should equal (5)
+    }
+    
+    it should "remote" in {
+        val repo = Git.init(path)
+        repo.remotes should have size (0)
+        
+        repo.addRemote("origin", "/path/to/remote.git")
+        repo.remotes should have size (1)
+        
+        repo.addRemote("github", "githuburl.git")
+        repo.remotes should have size (2)
+        
+        val remotes = repo.remotes.map(_.name)
+        remotes should contain ("origin")
+        remotes should contain ("github")
+        
+        val sameRepo = new GitRepository(repo.path)
+        sameRepo.remotes should have size (2)
+    }
+    
+    it should "push" in {
+        val repo = Git.init(randomPath)
+        writeFile(repo.path / "README", "Some readme text")
+        repo.add("README")
+        repo.commit("init")
+        
+        val remote = Git.init(randomPath)
+        evaluating { remote.history } should produce [NoHeadException]
+        
+        repo.addRemote("origin", remote.path)
+        repo.push()
+        
+        remote.history.size should equal (1)
+    }
+    
+    it should "pull" in {
+        val source = Git.init(randomPath)
+        writeFile(source.path / "README", "Remote repo README")
+        source.add("README")
+        source.commit("initial")
+    
+        val target = Git.init(randomPath)
+        target.addRemote("origin", source.path)
+        target.pull
+    
+        target.history.size should equal (1)
+    }
+    
+    it should "branch" in {
+        val repo = Git.init(randomPath)
+        writeFile(repo.path / "README", "Some readme text")
+        repo.add("README")
+        repo.commit("init")
+        repo.branchList should have size (1)
+        repo.branchList.map(_.name.split("/").last) should contain ("master")
+        
+        repo.branch("feature")
+        repo.branchList should have size (2)
+        val branches = repo.branchList.map(_.name.split("/").last)
+        branches should contain ("master")
+        branches should contain ("feature")
     }
 
+    it should "checkout" in {
+        val repo = Git.init(randomPath)
+        writeFile(repo.path / "README", "Some readme text")
+        repo.add("README")
+        repo.commit("init")
+        
+        repo.currentBranch should equal ("master")
+        
+        repo.branch("feature")
+        repo.checkout("feature")
+        
+        writeFile(repo.path / "README", "Some readme text added only in feature branch")
+        repo.add("README")
+        repo.commit("changed README")
+        
+        repo.history.size should equal (2)
+        
+        repo.checkout("master")
+        repo.history.size should equal (1)
+    }
+    
+    it should "single commit" in {
+        val repo = Git.init(randomPath)
+        writeFile(repo.path / "README", "Some readme text")
+        repo.add("README")
+        
+        val me = new Author("T. Tobolski", "teamon@example.com")
+        val him = new Author("D. Dettlaff", "dmilith@example.com")
+        repo.commit("init", author = me, committer = him)
+        
+        val commit = repo.history.next
+        commit.message should equal ("init")
+        commit.author.name should equal ("T. Tobolski")
+        commit.author.email should equal ("teamon@example.com")
+        commit.committer.name should equal ("D. Dettlaff")
+        commit.committer.email should equal ("dmilith@example.com")
+        
+        commit.sha should equal (repo.head.sha)
+    }
+    
+    "Author" should "init" in {
+        val author = new Author("T. Tobolski", "teamon@example.com")
+        author.name should equal ("T. Tobolski")
+        author.email should equal ("teamon@example.com")
+    }
 }
