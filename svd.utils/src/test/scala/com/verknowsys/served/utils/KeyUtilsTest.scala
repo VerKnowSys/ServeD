e1994e1e5d38e2b18de549e8aaf2323109d0a639
Tymon Tobolski <i@teamon.eu>
2011-06-30 23:13:41 +0200

    Moved testing utils to test codebase

--- svd.utils/src/test/scala/com/verknowsys/served/utils/KeyUtilsTest.scala
+++ svd.utils/src/test/scala/com/verknowsys/served/utils/KeyUtilsTest.scala
@@ -1,13 +1,11 @@
 package com.verknowsys.served.utils
 
 import com.verknowsys.served.testing._
-import scala.io.Source
 import sun.security.rsa.RSAPublicKeyImpl
 
 class KeyUtilsTest extends DefaultTest {
     "KeyUtils" should "parse public key" in {
-        val file = Source.fromURL(getClass.getResource("/test_key_rsa.pub"))
-        val data = file.getLines.mkString("\n")
+        val data = testPublicKey
         
         val key = KeyUtils.load(data)
         key should be ('defined)
