ce915315b6fc71ef0eee2c6e61d0f94c099ace0f
Tymon Tobolski <i@teamon.eu>
2011-06-30 23:21:28 +0200

    Improved KeyUtils test

--- svd.utils/src/test/scala/com/verknowsys/served/utils/KeyUtilsTest.scala
+++ svd.utils/src/test/scala/com/verknowsys/served/utils/KeyUtilsTest.scala
@@ -4,11 +4,16 @@ import com.verknowsys.served.testing._
 import sun.security.rsa.RSAPublicKeyImpl
 
 class KeyUtilsTest extends DefaultTest {
-    "KeyUtils" should "parse public key" in {
+    "KeyUtils" should "parse valid public key" in {
         val data = testPublicKey
         
         val key = KeyUtils.load(data)
         key should be ('defined)
         key.get.getClass should equal (classOf[RSAPublicKeyImpl])
+    }
+    
+    it should "return None for invalid key in" in {
+        val key = KeyUtils.load("some weird key")
+        key should equal (None)
     }
 }
