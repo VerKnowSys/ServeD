98e64187ea8489fdaf018e9f6c717d8902b8a014
Tymon Tobolski <i@teamon.eu>
2011-07-15 01:46:03 +0200

    Introduced $ function for API requests

--- svd.web/src/main/scala/com/verknowsys/served/web/lib/Session.scala
+++ svd.web/src/main/scala/com/verknowsys/served/web/lib/Session.scala
@@ -7,9 +7,13 @@ import akka.actor.{Actor, ActorRef}
 import com.verknowsys.served.utils.Logging
 import com.verknowsys.served.api._
 
+object $ {
+    def apply[T](msg: ApiMessage)(f: PartialFunction[Any, T]) = Session.api.request(msg)(f)
+}
+
 object Session extends Logging {
     object UserID extends SessionVar[Int](-1)
-    
+
     object api {
         final val host = "localhost" // TODO: Use some configuration
         final val port = 10 // 2011-06-09 02:01:28 - dmilith - TODO: XXX: should use config value for standard value of API port
@@ -18,38 +22,38 @@ object Session extends Logging {
 
         def request[T](msg: ApiMessage)(f: PartialFunction[Any, T]): Option[T] = {
             log.debug("API REQUEST: %s", msg)
-            
+
             val response = svd.get !! msg
             log.debug("API RESPONSE: %s", response)
-            
+
             response match {
                 case Some(response) if f.isDefinedAt(response) => Some(f(response))
                 case Some(response) => None //logger.error("[akka] Unhandled reponse %s", response)
                 case None => None//logger.error("[akka] Connection timeout")
             }
         }
-        
+
         protected def service = RemoteSession(host, port).get // HACK: Handle error!!
     }
 
     def login(userUid: Int, password: String) = {
-        api.request(General.Connect(userUid)) {
+        $(General.Connect(userUid)) {
             case Success =>
                 UserID.set(userUid) // XXX: hacky but all here is one big hack right now
             case Error =>
                 //logger.error("Failed to log in")
         }.isDefined
     }
-    
+
     def logout = UserID.set(-1)
-    
+
     def authorized = {
-        // TODO: Add some real validation. Temporary code so there is no need to log in on every code reload 
+        // TODO: Add some real validation. Temporary code so there is no need to log in on every code reload
         // (jetty restarts app and session vars are gone)
         if(UserID.get == -1){
             login(501, "")
         }
-        
+
         UserID.get != -1 // change me!
     }
 }
--- svd.web/src/main/scala/com/verknowsys/served/web/snippet/Git.scala
+++ svd.web/src/main/scala/com/verknowsys/served/web/snippet/Git.scala
@@ -7,19 +7,21 @@ import net.liftweb.http.js._
 
 import scala.xml._
 
-import com.verknowsys.served.web.lib.Session
+import com.verknowsys.served.web.lib._
 import com.verknowsys.served.api.git._
 import com.verknowsys.served.api.Success
 import com.verknowsys.served.utils.Logging
 
+
 object AddRepository extends LiftScreen {
     val name = field("Name", "", trim, valMinLen(1, "Name can not be blank"))
-    
-    override def finishButton: Elem = <button>{"Add repository"}</button>  
-    
+
+    override def finishButton: Elem = <button>{"Add repository"}</button>
+
     def finish() {
-        Session.api.request(CreateRepository(name.is)){
-            case repo: Repository => S.notice("Repository " + repo.name + " created")
+        $(CreateRepository(name.is)){
+            case repo: Repository =>
+                S.notice("Repository " + repo.name + " created")
         }
     }
 }
@@ -27,13 +29,13 @@ object AddRepository extends LiftScreen {
 class GitSnippet extends Logging {
     def listRepositories = {
         log.trace("Listing repositories")
-        val repositories = Session.api.request(ListRepositories){ case Repositories(repositories) => repositories }.getOrElse(Nil)
+        val repositories = $(ListRepositories){ case Repositories(repositories) => repositories }.getOrElse(Nil)
         ".row *" #> repositories.map { repo =>
             val id = nextFuncName
             ".name [id]" #> id &
             ".name *"  #> repo.name &
             ".remove *"     #> SHtml.a(() => {
-                Session.api.request(RemoveRepository(repo.uuid)){
+                $(RemoveRepository(repo.uuid)){
                     case Success => JE.JsRaw("$('#"+id+"').parent().remove()").cmd
                 }.getOrElse(JsCmds.Noop)
             }, Text("Remove"))
