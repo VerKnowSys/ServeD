b997604b563bd083f869b619752c19be03be1bed
Tymon Tobolski <i@teamon.eu>
2011-07-25 21:25:57 +0200

    SLF4J binding for svd logger

---
+++ svd.web/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -0,0 +1,31 @@
+package org.slf4j.impl;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+
+public class StaticLoggerBinder implements LoggerFactoryBinder {
+    private static final StaticLoggerBinder SINGLETON = new StaticLoggerBinder();
+
+    public static final StaticLoggerBinder getSingleton() {
+        return SINGLETON;
+    }
+
+    public static String REQUESTED_API_VERSION = "1.6";  // !final
+
+    private static final String loggerFactoryClassStr = SvdSFL4JLoggerFactory.class.getName();
+
+    private final ILoggerFactory loggerFactory;
+
+    private StaticLoggerBinder() {
+        loggerFactory = new SvdSFL4JLoggerFactory();
+    }
+
+    public ILoggerFactory getLoggerFactory() {
+        return loggerFactory;
+    }
+
+    public String getLoggerFactoryClassStr() {
+        return loggerFactoryClassStr;
+    }
+}
