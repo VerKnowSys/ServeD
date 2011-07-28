5e487fb9381145480494922de0653afe37b69d98
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-07-28 13:36:38 +0200

    Removed accidentally committed changes for old_svd.web
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- old_svd.web/src/main/scala/com/verknowsys/served/web/lib/package.scala
+++
@@ -1,9 +0,0 @@
-package com.verknowsys.served.web
-
-import com.verknowsys.served.api.ApiMessage
-
-package object lib {
-    implicit def ApiMessage2Requst(msg: ApiMessage) = new {
-        def <>[T](pf: PartialFunction[Any, T]) = Session.api.request(msg)(pf)
-    }
-}
