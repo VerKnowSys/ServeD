8177f9ff276e63f0c34fe4fc44b8bbdb1f1d7f47
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-08-13 23:53:31 +0200

    Stub for GlobalRegistry
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ svd.core/src/main/scala/com/verknowsys/served/GlobalRegistry.scala
@@ -0,0 +1,10 @@
+package com.verknowsys.served
+
+/**
+ *  @author dmilith
+ *
+ *   Global synchronized registry of user data which are host related.. bla bla
+ */
+object GlobalRegistry {
+    
+}
