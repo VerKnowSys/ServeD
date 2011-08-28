eb0324c3c4d2a2f7fe219c2090d86773b239dbca
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-08-28 22:37:06 +0200

    Base template for passenger in user services. Improved validation.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.user/src/main/scala/com/verknowsys/served/services/SvdUserServices.scala
+++ svd.user/src/main/scala/com/verknowsys/served/services/SvdUserServices.scala
@@ -10,6 +10,48 @@ import com.verknowsys.served.api._
 
 object SvdUserServices {
 
+
+    def passengerDefinitionTemplate(
+        account: SvdAccount, defaultDomainName: String = SvdConfig.defaultDomain) =
+"""
+worker_processes    %s;
+
+events {
+    worker_connections  %s;
+}
+
+http {
+    include     mime.types;
+    default_type    application/octet-stream;
+    keepalive_timeout   %s;
+    gzip    on;
+
+    server {
+        listen  %s;
+        server_name %s;
+        access_log  logs/%s.access.log;
+        location    /   {
+            root    %s;
+            index   index.html  index.htm;
+        }
+        error_page  500    502    503    504    /50x.html;
+        location    =  /50x.html {
+            root    %shtml;
+        }
+    }
+}
+
+""".format(
+    SvdConfig.defaultHttpAmountOfWorkers, /* worker_processes */
+    SvdConfig.defaultHttpWorkerConnections, /* worker_connections */
+    SvdConfig.defaultHttpKeepAliveTimeout, /* keepalive_timeout */
+    8181, /* listen */ // XXX:, HACK:
+    "localhost", /* server name */
+    defaultDomainName, /* access log */
+    SvdConfig.userHomeDir / "%s".format(account.uid) / "Public", /* root */
+    SvdConfig.publicHttpDir /* root */
+)
+
     /**
      *  @author dmilith
      *
@@ -18,37 +60,42 @@ object SvdUserServices {
      */
      def passengerConfig(account: SvdAccount, name: String = "Passenger") = SvdServiceConfig(
 
-         name = name,
+        name = name,
 
-         install = SvdShellOperation(
-            "mkdir -p %s/Apps ; cp -R %s%s-** %s/Apps/%s && echo install".format(
-                    SvdConfig.userHomeDir / account.uid.toString,
-                    SvdConfig.softwareRoot,
-                    name,
-                    SvdConfig.userHomeDir / account.uid.toString,
-                    name),
+        install = SvdShellOperation(
+            "mkdir -p %s ; cp -R %s-** %s && echo install".format(
+                    SvdConfig.userHomeDir / account.uid.toString / "Apps", /* mkdir */
+                    SvdConfig.softwareRoot / name,
+                    SvdConfig.userHomeDir / account.uid.toString / "Apps" / name),
                 waitForOutputFor = 90,
                 expectStdOut = List("install")) :: Nil,
 
-         // configure = SvdShellOperation() :: Nil,
+        configure = SvdShellOperation(
+           "mkdir %s ; echo \"%s\" > %s".format(
+                SvdConfig.userHomeDir / "%s".format(account.uid) / "Public",
+                passengerDefinitionTemplate(account),
+                SvdConfig.userHomeDir / "%s".format(account.uid) / "Apps" / name / "conf" / "nginx.conf")) :: Nil,
 
-         validate = SvdShellOperation(
-            "test -e %s/Apps/%s/sbin/nginx && echo validate".format(
-                    SvdConfig.userHomeDir / account.uid.toString,
+        validate = SvdShellOperation(
+            "%s/sbin/nginx -p %s/ -t".format(
+                    SvdConfig.userHomeDir / account.uid.toString / "Apps" / name,
+                    SvdConfig.userHomeDir / "%s".format(account.uid) / "Apps" / name,
                     name),
-                waitForOutputFor = 60,
-                expectStdOut = List("validate")) :: Nil,
+                waitForOutputFor = 10,
+                expectStdErr = List("test is successful")) :: Nil,
 
-         start = SvdShellOperation(
-            "%s/Apps/%s/sbin/nginx && echo start".format(
-                    SvdConfig.userHomeDir / account.uid.toString,
+        start = SvdShellOperation(
+            "%s/sbin/nginx -p %s/ && echo start".format(
+                    SvdConfig.userHomeDir / account.uid.toString / "Apps" / name,
+                    SvdConfig.userHomeDir / "%s".format(account.uid) / "Apps" / name,
                     name),
                 waitForOutputFor = 30,
                 expectStdOut = List("start")) :: Nil,
 
-         stop = SvdShellOperation(
-            "%s/Apps/%s/sbin/nginx -s stop && echo stop".format(
-                SvdConfig.userHomeDir / account.uid.toString,
+        stop = SvdShellOperation(
+            "%s/sbin/nginx -p %s/ -s stop && echo stop".format(
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name,
+                SvdConfig.userHomeDir / "%s".format(account.uid) / "Apps" / name,
                 name),
             waitForOutputFor = 15,
             expectStdOut = List("stop")) :: Nil
