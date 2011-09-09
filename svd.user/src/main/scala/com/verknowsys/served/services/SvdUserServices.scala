9e8dd26a909a8214467d780e7a56c18fd78bfc27
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2011-09-09 18:36:49 +0200

    Definition for PostgreSQL user side application.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.user/src/main/scala/com/verknowsys/served/services/SvdUserServices.scala
+++ svd.user/src/main/scala/com/verknowsys/served/services/SvdUserServices.scala
@@ -18,7 +18,91 @@ import com.verknowsys.served.api._
 object SvdUserServices {
 
 
-     def rackWebAppConfig(account: SvdAccount, domain: SvdUserDomain, name: String = "Passenger") = SvdServiceConfig(
+    def postgresDatabaseConfig(account: SvdAccount, name: String = "PostgreSQL") = SvdServiceConfig(
+        name = name,
+
+        install = SvdShellOperation(
+            "mkdir -p %s ; cp -R %s-** %s && %s -D %s && echo install".format(
+                SvdConfig.userHomeDir / account.uid.toString / "Apps", /* mkdir */
+                SvdConfig.softwareRoot / name,
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name, /* cp */
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / "bin" / "initdb",
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / name /* data folder */
+            ),
+            waitForOutputFor = 90,
+            expectStdOut = List("install")) :: Nil,
+
+        validate = SvdShellOperation(
+            "test -x %s && test -x %s && test -x %s && echo validation".format(
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / "bin" / "initdb",
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / "bin" / "pg_ctl",
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / "bin" / "postgres"
+            ),
+            waitForOutputFor = 5,
+            expectStdOut = List("validation")) :: Nil,
+
+        configure = SvdShellOperation(
+            "echo %s > %s && echo %s > %s && echo configuration".format(
+                """
+                local all all trust
+                host replication all 0.0.0.0/0 trust
+                """,
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / name / "pg_hba.conf", /* pg_hba.conf */
+                """
+                unix_socket_directory %s
+                max_connections = 10
+                checkpoint_segments = 24
+                password_encryption = on
+                datestyle = 'iso, mdy'
+                shared_buffers = 64MB
+                temp_buffers = 32MB
+                work_mem = 16MB
+                max_stack_depth = 16MB
+
+                logging_collector = true
+                log_directory = %s
+                client_encoding = 'UTF-8'
+                lc_messages = 'pl_PL.UTF-8'
+                lc_monetary = 'pl_PL.UTF-8'
+                lc_numeric = 'pl_PL.UTF-8'
+                lc_time = 'pl_PL.UTF-8'
+                default_text_search_config = 'pg_catalog.polish'
+
+                # master for host standby, streaming replication:
+                wal_level = hot_standby
+                max_wal_senders = 5
+                wal_keep_segments = 32
+                archive_mode = on
+                # archive_command = '#{CP_BIN} %p __POSTGRES_WAL_DIR__/%f'
+                """.format(
+                    SvdConfig.temporaryDir / "%s-%s.socket".format(name, account.uuid),
+                    SvdConfig.temporaryDir / "%s-%s.log".format(name, account.uuid) // NOTE: consider log in app dir
+                ),
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / name / "postgresql.conf" /* postgresql.conf */
+            ),
+            waitForOutputFor = 5,
+            expectStdOut = List("configuration")) :: Nil,
+
+        start = SvdShellOperation(
+            "%s -m fast -D %s start && echo start".format(
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / "bin" / "pg_ctl",
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / name /* data folder */
+            ),
+            waitForOutputFor = 90,
+            expectStdOut = List("start")) :: Nil,
+
+        stop = SvdShellOperation(
+            "%s -m fast -D %s stop && echo stop".format(
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / "bin" / "pg_ctl",
+                SvdConfig.userHomeDir / account.uid.toString / "Apps" / name / name /* data folder */
+            ),
+            waitForOutputFor = 15,
+            expectStdOut = List("stop")) :: Nil
+
+    )
+
+
+    def rackWebAppConfig(account: SvdAccount, domain: SvdUserDomain, name: String = "Passenger") = SvdServiceConfig(
         /*      appRoot     =>  SvdConfig.userHomeDir / account.uid.toString / "WebApps" / domain.name    */
         name = name,
 
