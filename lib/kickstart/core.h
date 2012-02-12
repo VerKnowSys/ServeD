9ce3235abee2b10b670ea58f8f0af8cf9dd72520
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-02-12 15:51:12 +0100

    Done usage_sys code for BSD systems.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- lib/kickstart/core.h
+++ lib/kickstart/core.h
@@ -14,6 +14,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <iomanip>
+
 #ifdef __linux__
     #include <string.h>
 #endif
--- lib/kickstart/usage_sys.cc
+++ lib/kickstart/usage_sys.cc
@@ -7,63 +7,71 @@
 
 
 #include <kvm.h>
-#include <fcntl.h>
 #include <sys/sysctl.h>
 #include <sys/user.h>
 #include <sys/proc.h>
 #include <paths.h>
-#include <stdlib.h>
-#include <iomanip>
 
 #define ord(c) ((int)(unsigned char)(c))
 
 
-int getProcessUsage(int uid, bool consoleOutput) {
+extern "C" {
 
-    int count = 0;
-    char** args = NULL;
-    string command;
 
-    kvm_t* kd = kvm_open(NULL, "/dev/mem", NULL, O_RDONLY, "kvm_open");
-    if (kd == 0) {
-        cerr << "Error initializing kernel descriptor" << endl;
-        return -1;
-    }
 
-    kinfo_proc* procs;
-    // for (int g = 0; g < 100; g++)
-        procs = kvm_getprocs(kd, KERN_PROC_UID, uid, &count); // get process directly from kernel
+    const char* getProcessUsage(int uid, bool consoleOutput = false) {
 
-    if (consoleOutput)
-        cout << "Process count: " << count << ". Owner UID: " << uid << endl;
+        int count = 0;
+        char** args = NULL;
+        string command, output;
 
-    cout << setiosflags(ios::left)
-        << setw(6) << "| NO:"
-        << setw(27) << "| NAME:"
-        << setw(52) << "| CMD:"
-        << setw(10) << "| PID:"
-        << setw(10) << "| PPID:"
-        << setw(10) << "| RSS:"
-        << setw(10) << "| MRSS:"
-        << setw(16) << "| RUN-TIME(ms):"
-        << setw(12) << "| BLK-IN:"
-        << setw(12) << "| BLK-OUT:"
-        << setw(6) << "| THR:"
-        << setw(6) << "| PRI-NRML:"
-        << endl;
+        kvm_t* kd = kvm_open(NULL, "/dev/mem", NULL, O_RDONLY, "kvm_open");
+        if (kd == 0) {
+            if (consoleOutput)
+                cerr << "Error initializing kernel descriptor!" << endl;
+            return (char*)"KDERR";
+        }
 
-    for (int i = 0; i < count; ++i) {
-        command = "";
-        args = kvm_getargv(kd, procs, 0);
+        kinfo_proc* procs;
+        // for (int yy = 0; yy < 1000; yy++)
+            procs = kvm_getprocs(kd, KERN_PROC_UID, uid, &count); // get processes directly from BSD kernel
 
-        for (int y = 0; (args != 0) && (args[y] != 0); y++)
-            if (y == 0)
-                command = string(args[y]);
-            else
-                command += " " + string(args[y]);
+        if (count <= 0) {
+            if (consoleOutput)
+                cerr << "No processes for given UID!" << endl;
+            return (char*)"NOPCS";
+        }
 
         if (consoleOutput) {
+            cout << "Process count: " << count << ". Owner UID: " << uid << endl;
             cout << setiosflags(ios::left)
+                << setw(6) << "| NO:"
+                << setw(27) << "| NAME:"
+                << setw(52) << "| CMD:"
+                << setw(10) << "| PID:"
+                << setw(10) << "| PPID:"
+                << setw(10) << "| RSS:"
+                << setw(10) << "| MRSS:"
+                << setw(16) << "| RUN-TIME(ms):"
+                << setw(12) << "| BLK-IN:"
+                << setw(12) << "| BLK-OUT:"
+                << setw(6) << "| THR:"
+                << setw(6) << "| PRI-NRML:"
+                << endl;
+        }
+
+        for (int i = 0; i < count; ++i) {
+            stringstream out;
+            command = "";
+            args = kvm_getargv(kd, procs, 0);
+
+            for (int y = 0; (args != 0) && (args[y] != 0); y++)
+                if (y == 0)
+                    command = string(args[y]);
+                else
+                    command += " " + string(args[y]);
+
+            out << setiosflags(ios::left)
                 << "| " << setw(4) << (i + 1)
                 << "| " << setw(25) << (procs->ki_comm)
                 << "| " << setw(50) << (command)
@@ -71,27 +79,33 @@ int getProcessUsage(int uid, bool consoleOutput) {
                 << "| " << setw(8) << (procs->ki_ppid)
                 << "| " << setw(8) << (procs->ki_rssize * 4)
                 << "| " << setw(8) << (procs->ki_rusage.ru_maxrss * 4)
-                << "| " << setw(14) << (procs->ki_runtime / 1000) // 1000000)
+                << "| " << setw(14) << (procs->ki_runtime / 1000)
                 << "| " << setw(10) << (procs->ki_rusage.ru_inblock)
                 << "| " << setw(10) << (procs->ki_rusage.ru_oublock)
-                // << "SWPS: " << (procs->ki_rusage.ru_nswap)
                 << "| " << setw(4) << (procs->ki_numthreads)
                 << "| " << setw(6) << ord(procs->ki_pri.pri_level)
-                // << "PRI-NTVE: " << ord(procs->ki_pri.pri_native)
-                // << "PRI-USER: " << ord(procs->ki_pri.pri_user)
                 << endl;
+
+            if (consoleOutput)
+                cout << out.str();
+
+            output += out.str();
+            procs++;
         }
 
-        procs++;
+        kvm_close(kd);
+        return output.c_str();
     }
-
-    kvm_close(kd);
-    return 0;
-}
+} // extern
 
 
 int main(int argc, char const *argv[]) {
-    getProcessUsage(0, true);
+
+    if (argc == 1) {
+        cerr << "No UID argument given!" << endl;
+        exit(NO_UID_GIVEN_ERROR);
+    }
+
+    getProcessUsage(atoi(argv[1]), true);
     return 0;
 }
-
