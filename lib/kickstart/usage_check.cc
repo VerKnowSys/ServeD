7d0f82f089261da7f75d6f090c1ae36927e6fe62
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-02-15 15:20:36 +0100

    Implemented heavy usage_check. It should show every kind of a problem possible from C libraries which would be used on ServeD side
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- lib/kickstart/usage_check.cc
+++ lib/kickstart/usage_check.cc
@@ -3,13 +3,56 @@
 
 int main(int argc, char const *argv[]) {
 
+    static const int TIMES = 25;
+
+    int argument = 0;
+    int count;
+
     if (argc == 1) {
-        cerr << "No UID argument given!" << endl;
-        exit(NO_UID_GIVEN_ERROR);
+        cout << "No UID argument given for getProcessUsage(). Setting default to uid 0." << endl;
+    } else {
+        argument = atoi(argv[1]);
     }
 
-    getProcessUsage(atoi(argv[1]), true);
-    
-    
+    for (int z = 0; z < TIMES; z++) {
+
+        int COUNT_TIMES = 1000;
+
+        cout << "Show values check: " << z << endl;
+        cout << getProcessUsage(argument, true) << endl;
+        cout << getProcessUsage(argument) << endl;
+        cout << getSocketUsage() << endl;
+
+        while (COUNT_TIMES > 0) {
+            timespec tS;
+
+            count = COUNT_TIMES;
+            tS.tv_sec = 0;
+            tS.tv_nsec = 0;
+            clock_settime(CLOCK_REALTIME, &tS);
+            while (count > 0) {
+                getProcessUsage(argument);
+                --count;
+            }
+            clock_gettime(CLOCK_REALTIME, &tS);
+            cout << "Time taken for " << COUNT_TIMES << " rounds of getProcessUsage() is: " << tS.tv_sec << "s, " << tS.tv_nsec/1000 << "us (" << tS.tv_nsec/1000000 << "ms)" << endl;
+
+
+            count = COUNT_TIMES;
+            tS.tv_sec = 0;
+            tS.tv_nsec = 0;
+            clock_settime(CLOCK_REALTIME, &tS);
+            while (count > 0) {
+                getSocketUsage();
+                --count;
+            }
+            clock_gettime(CLOCK_REALTIME, &tS);
+            cout << "Time taken for " << COUNT_TIMES << " rounds of getSocketUsage() is: " << tS.tv_sec << "s, " << tS.tv_nsec/1000 << "us (" << tS.tv_nsec/1000000 << "ms)" << endl;
+
+
+            COUNT_TIMES /= 2;
+        }
+    }
+
     return 0;
 }
