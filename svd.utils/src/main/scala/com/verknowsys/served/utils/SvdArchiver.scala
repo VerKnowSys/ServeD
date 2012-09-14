cf6aef216a22cec734fabb1d8bfc60d9df41bded
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-09-14 17:56:40 +0200

    First working version of SvdArchiver watching file changes in already existing ZIP files.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.utils/src/main/scala/com/verknowsys/served/utils/SvdArchiver.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/utils/SvdArchiver.scala
@@ -4,10 +4,41 @@
 package com.verknowsys.served.utils
 
 import com.verknowsys.served._
-import de.schlichtherle.io._;
+import de.schlichtherle.io._
 import de.schlichtherle.key._
-import de.schlichtherle.crypto.io.raes._;
-import de.schlichtherle.io.File;
+import de.schlichtherle.crypto.io.raes._
+import java.io.File
+import de.schlichtherle.io.{File => TFile}
+
+
+/**
+    @author dmilith
+    AES Key provider
+*/
+class SvdAESKeyProvider extends AesKeyProvider {
+
+    def getCreateKey = SvdConfig.backupKey.toCharArray
+
+    def getOpenKey = SvdConfig.backupKey.toCharArray
+
+    def invalidOpenKey = {
+        // This method is called whenever a key for an existing protected resource is invalid.
+        throw new UnsupportedOperationException("Cannot handle invalid keys!")
+    }
+
+    def getKeyStrength = AesKeyProvider.KEY_STRENGTH_128
+
+    def setKeyStrength(keyStrength: Int) = AesKeyProvider.KEY_STRENGTH_128
+}
+
+
+/**
+    @author dmilith
+    AES Key manager
+*/
+class SvdArchiverKeyManager extends KeyManager {
+    mapKeyProviderType(classOf[AesKeyProvider], classOf[SvdAESKeyProvider])
+}
 
 
 /**
@@ -16,39 +47,8 @@ import de.schlichtherle.io.File;
 */
 object SvdArchiver {
 
-
-    /**
-        @author dmilith
-        AES Key provider
-    */
-    class SvdAESKeyProvider extends AesKeyProvider {
-
-        def getCreateKey = SvdConfig.backupKey.toCharArray
-
-        def getOpenKey = SvdConfig.backupKey.toCharArray
-
-        def invalidOpenKey = {
-            // This method is called whenever a key for an existing protected resource is invalid.
-            throw new UnsupportedOperationException("Cannot handle invalid keys!")
-        }
-
-        def getKeyStrength = AesKeyProvider.KEY_STRENGTH_128
-
-        def setKeyStrength(keyStrength: Int) = AesKeyProvider.KEY_STRENGTH_128
-    }
-
-
-    /**
-        @author dmilith
-        AES Key manager
-    */
-    class SvdArchiverKeyManager extends KeyManager {
-        mapKeyProviderType(classOf[AesKeyProvider], classOf[SvdAESKeyProvider])
-    }
-
-
     System.setProperty("de.schlichtherle.key.KeyManager", SvdConfig.defaultBackupKeyManager)
-    File.setDefaultArchiveDetector(new DefaultArchiveDetector(SvdConfig.defaultBackupFileExtension))
+    TFile.setDefaultArchiveDetector(new DefaultArchiveDetector(SvdConfig.defaultBackupFileExtension))
 
 
     /**
@@ -60,11 +60,46 @@ object SvdArchiver {
         trimmedFileName.matches(SvdConfig.defaultBackupFileMatcher) match {
             case true =>
                 // case 1: decompression cause matched extension found
-                new File(fileOrDirectoryPath).archiveCopyAllTo(new File("%s".format(unpackDir)), ArchiveDetector.DEFAULT, ArchiveDetector.NULL)
+                new TFile(fileOrDirectoryPath).archiveCopyAllTo(new File("%s".format(unpackDir)), ArchiveDetector.DEFAULT, ArchiveDetector.NULL)
 
             case false =>
                 // case 2: compression of whatever given as path
-                new File(fileOrDirectoryPath).archiveCopyAllTo(new File("%s%s.%s".format(SvdConfig.defaultBackupDir, trimmedFileName, SvdConfig.defaultBackupFileExtension)))
+                val sourceFiles = new TFile(fileOrDirectoryPath).listFiles
+                val sourceDirs = sourceFiles.filter{_.isDirectory}
+
+                def gatherAllDirsRecursively(rootDir: List[File], gathered: List[File] = Nil): List[File] = {
+                    if (rootDir.isEmpty)
+                        gathered
+                    else {
+                        val dir = rootDir.head
+                        val dirList = dir.listFiles.filter{_.isDirectory}.toList
+                        if (dirList.isEmpty)
+                            gatherAllDirsRecursively(rootDir.tail, gathered ++ List(dir))
+                        else
+                            gatherAllDirsRecursively(rootDir.tail ++ dirList, gathered ++ List(dir))
+                    }
+                }
+
+                for {
+                    source <- gatherAllDirsRecursively(sourceDirs.toList).map{_.asInstanceOf[TFile]}
+                } yield {
+                    val destinationFiles = "%s%s.%s%s".format(SvdConfig.defaultBackupDir, trimmedFileName, SvdConfig.defaultBackupFileExtension, source.getPath.split(trimmedFileName).tail.mkString("/"))
+
+                    if (destinationFiles != null) {
+                        for {
+                            src <- source.listFiles.filter{_.isFile}
+                            dst <- new TFile(destinationFiles).listFiles.filter{_.getName.matches(src.getName)}
+                        } yield {
+                            if ((src.getName == dst.getName) && (src.lastModified/10000 != dst.lastModified/10000)) {
+                                println("Changed: %s -> %s".format(src, dst))
+                                TFile.cp_p(src, dst)
+                            }
+                        }
+                    } else {
+                        println("DESTINATION NOT NULL")
+                    }
+                }
+
         }
     }
 
