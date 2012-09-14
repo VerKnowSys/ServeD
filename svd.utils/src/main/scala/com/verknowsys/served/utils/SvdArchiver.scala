726f0f285ac723b503c52183a6f2d5ec17b0041c
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-09-14 19:14:15 +0200

    Done optimization in archive check in SvdArchiver. Preparing for changes to detect removed/new files too.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.utils/src/main/scala/com/verknowsys/served/utils/SvdArchiver.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/utils/SvdArchiver.scala
@@ -55,7 +55,7 @@ object SvdArchiver {
         @author dmilith
         Simple hook for compressing and decompressing AES ZIP archives.
     */
-    def apply(fileOrDirectoryPath: String, unpackDir: String = SvdConfig.defaultBackupDir + "/CURRENT") = {
+    def apply(fileOrDirectoryPath: String, unpackDir: String = SvdConfig.defaultBackupDir + "/CURRENT") {
         val trimmedFileName = fileOrDirectoryPath.split("/").last
         trimmedFileName.matches(SvdConfig.defaultBackupFileMatcher) match {
             case true =>
@@ -68,35 +68,51 @@ object SvdArchiver {
                 val sourceDirs = sourceFiles.filter{_.isDirectory}
 
                 def gatherAllDirsRecursively(rootDir: List[File], gathered: List[File] = Nil): List[File] = {
-                    if (rootDir.isEmpty)
+                    if (rootDir.isEmpty) // TODO: do pattern match instead of if
                         gathered
                     else {
                         val dir = rootDir.head
                         val dirList = dir.listFiles.filter{_.isDirectory}.toList
-                        if (dirList.isEmpty)
+                        if (dirList.isEmpty) // TODO :pattern match instead of if
                             gatherAllDirsRecursively(rootDir.tail, gathered ++ List(dir))
                         else
                             gatherAllDirsRecursively(rootDir.tail ++ dirList, gathered ++ List(dir))
                     }
                 }
 
-                for {
-                    source <- gatherAllDirsRecursively(sourceDirs.toList).map{_.asInstanceOf[TFile]}
-                } yield {
-                    val destinationFiles = "%s%s.%s%s".format(SvdConfig.defaultBackupDir, trimmedFileName, SvdConfig.defaultBackupFileExtension, source.getPath.split(trimmedFileName).tail.mkString("/"))
+                // check for archive existance:
+                val virtualRootDestination = "%s%s.%s".format(SvdConfig.defaultBackupDir, trimmedFileName, SvdConfig.defaultBackupFileExtension)
+                if (new TFile(virtualRootDestination).listFiles == null) {
+                    println("No archive found. Creating new one")
+                    new TFile(fileOrDirectoryPath).archiveCopyAllTo(new TFile("%s%s.%s".format(SvdConfig.defaultBackupDir, trimmedFileName, SvdConfig.defaultBackupFileExtension)))
 
-                    if (destinationFiles != null) {
+                } else {
+                    println("Found archive. Checking for changes")
+                    // perform check on existing archive, and try to do update:
+                    val gatheredDirList = gatherAllDirsRecursively(sourceDirs.toList).map{_.asInstanceOf[TFile]}
+
+                    for {
+                        source <- gatheredDirList
+                    } yield {
+                        val destinationFiles = "%s%s.%s%s".format(SvdConfig.defaultBackupDir, trimmedFileName, SvdConfig.defaultBackupFileExtension, source.getPath.split(trimmedFileName).tail.mkString("/"))
+
+
                         for {
                             src <- source.listFiles.filter{_.isFile}
                             dst <- new TFile(destinationFiles).listFiles.filter{_.getName.matches(src.getName)}
                         } yield {
                             if ((src.getName == dst.getName) && (src.lastModified/10000 != dst.lastModified/10000)) {
-                                println("Changed: %s -> %s".format(src, dst))
+                                println("Changed and will be updated: %s -> %s".format(src, dst))
                                 TFile.cp_p(src, dst)
                             }
+                            // val unfiltered = new TFile(destinationFiles).listFiles.toList
+                            // val srcFile = src.getPath.split(trimmedFileName).tail.mkString("/")
+                            // val listOfNonExistantFiles = unfiltered.filterNot{_.getPath.contains(srcFile)}
+                            // if (listOfNonExistantFiles.length == 0) {
+                            //     println("New file/ No file: %s".format(srcFile))
+                            //     // TFile.cp_p(src, dst)
+                            // }
                         }
-                    } else {
-                        println("DESTINATION NOT NULL")
                     }
                 }
 
