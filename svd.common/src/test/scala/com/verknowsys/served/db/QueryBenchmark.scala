606accb67b9908f674615dc56211c392c0780a2c
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-09-29 03:57:31 +0200

    Query benchmark will use svd logger instead of println.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.common/src/test/scala/com/verknowsys/served/db/QueryBenchmark.scala
+++ svd.common/src/test/scala/com/verknowsys/served/db/QueryBenchmark.scala
@@ -1,7 +1,7 @@
 package com.verknowsys.served.db
 
+import com.verknowsys.served.testing._
 import com.verknowsys.served.utils.Benchmark._
-import com.verknowsys.served.SvdSpecHelpers._
 import com.verknowsys.served.utils._
 import com.verknowsys.served._
 
@@ -14,93 +14,90 @@ import org.neodatis.odb.core.query.nq.SimpleNativeQuery
 case class Foo(val uuid: UUID = randomUUID)
 
 
-object QueryBenchmark {
+object QueryBenchmark extends Logging {
     def main(args: Array[String]): Unit = {
         val dbname = tmpDir / "svd_bench/svd_db_query_benchmark.neodatis"
         rmdir(tmpDir / "svd_bench")
-        
+
         val server = ODBFactory.openServer(randomPort)
 
         server.addBase("base", dbname)
         server.startServer(true)
-        
-        
-        
-        
-        println("Storing objects")
-        
+
+
+
+
+        log.debug("Storing objects")
+
         var odb = server.openClient("base")
-        
+
         val objects = (1 to 10000) map { i => new Foo }
-    
+
         (1 to 100) foreach { i =>
             objects foreach { o => odb.store(o) }
         }
-        
+
         odb.close
         odb = server.openClient("base")
-        
+
         val first = objects.head
-        
-        
-        
-        
-        println("Testing results")
-        
-        println(odb.getObjects(new SimpleNativeQuery {
+
+
+
+
+        log.debug("Testing results")
+
+        log.debug("%s".format(odb.getObjects(new SimpleNativeQuery {
             def `match`(obj: Foo): Boolean = {
                 obj.uuid.compareTo(first.uuid) == 0
             }
-        }))
-        
-        println(odb.getObjects(new SimpleNativeQuery {
+        })))
+
+        log.debug("%s".format(odb.getObjects(new SimpleNativeQuery {
             def `match`(obj: Foo): Boolean = {
                 obj.uuid.getMostSignificantBits == first.uuid.getMostSignificantBits &&
                     obj.uuid.getLeastSignificantBits == first.uuid.getLeastSignificantBits
             }
-        }))
-        
+        })))
 
-        println(odb.getObjects(new CriteriaQuery(classOf[Foo], 
+
+        log.debug("%s".format(odb.getObjects(new CriteriaQuery(classOf[Foo],
             Where.and()
                 .add(Where.equal("uuid.mostSigBits", first.uuid.getMostSignificantBits))
                 .add(Where.equal("uuid.leastSigBits", first.uuid.getLeastSignificantBits))
-        )))
-        
-        
-        
-        
-        println("Benchmarking query")
-        
+        ))))
+
+        log.debug("Benchmarking query")
+
         odb.close
         odb = server.openClient("base")
-        
+
         val nq1 = new SimpleNativeQuery {
             def `match`(obj: Foo): Boolean = {
                 obj.uuid.compareTo(first.uuid) == 0
             }
         }
-        
+
         val nq2 = new SimpleNativeQuery {
             def `match`(obj: Foo): Boolean = {
                 obj.uuid.getMostSignificantBits == first.uuid.getMostSignificantBits &&
                     obj.uuid.getLeastSignificantBits == first.uuid.getLeastSignificantBits
             }
         }
-        
-        val cq = new CriteriaQuery(classOf[Foo], 
+
+        val cq = new CriteriaQuery(classOf[Foo],
             Where.and()
                 .add(Where.equal("uuid.mostSigBits", first.uuid.getMostSignificantBits))
                 .add(Where.equal("uuid.leastSigBits", first.uuid.getLeastSignificantBits))
         )
 
-        
+
         benchmark(100){
-            report("native query #1"){ 
+            report("native query #1"){
                 odb.getObjects(nq1)
                 ()
             } ::
-            report("native query #2"){ 
+            report("native query #2"){
                 odb.getObjects(nq2)
                 ()
             } ::
@@ -110,7 +107,7 @@ object QueryBenchmark {
             } ::
             Nil
         }
-        
+
         odb.close
     }
 }
