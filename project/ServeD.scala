63864a3127a11d997fd462255627e195fe596df1
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-10-14 01:29:30 +0200

    Brought back slf4japi dependency. Pending logger fix.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- project/ServeD.scala
+++ project/ServeD.scala
@@ -122,7 +122,7 @@ object Dependencies {
     // val jetty = "org.mortbay.jetty" % "jetty" % "6.1.22" % "container"
     // val jetty = "org.eclipse.jetty" % "jetty-webapp" % "7.4.1.v20110513"
     val sshd = "org.apache.sshd" % "sshd-core" % "0.7.0"
-    // val slf4japi = "org.slf4j" % "slf4j-api" % "1.7.1" // WARN: api change
+    val slf4japi = "org.slf4j" % "slf4j-api" % "1.7.1" // WARN: api change
     val commonsio = "commons-io" % "commons-io" % "1.3.2"
     val webbit = "org.webbitserver" % "webbit" % "0.4.14"
     // val scalaz = "org.scalaz" % "scalaz-core_2.9.2" % "7.0.0-M3"
@@ -189,7 +189,7 @@ object ServeD extends Build {
     lazy val utils = Project("utils", file("svd.utils"),
         settings = buildSettings ++ Seq(
             compileOrder        := CompileOrder.Mixed,
-            libraryDependencies ++= Seq(messadmin, jna, tzip, bouncycastle, sshd, liftUtil) // slf4japi/
+            libraryDependencies ++= Seq(messadmin, jna, tzip, bouncycastle, sshd, liftUtil, slf4japi)
         )
     ).settings(graph.Plugin.graphSettings: _*) dependsOn(api, testing % "test")
 
@@ -218,11 +218,4 @@ object ServeD extends Build {
         )
     ).settings(graph.Plugin.graphSettings: _*) dependsOn(api)
 
-
-    // publish for assembled jars:
-    // artifact in (Compile, assembly) ~= { art =>
-    //     art.copy(`classifier` = Some("assembly"))
-    // }
-
-    // addArtifact(artifact in (Compile, assembly), assembly)
 }
