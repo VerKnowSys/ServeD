5887a2aa8a7336284f5705157dbfdb852dedb0be
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-10-15 00:38:42 +0200

    Disabled postStop and preStart implementations in ExceptionHandler.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.utils/src/main/scala/com/verknowsys/served/utils/SvdExceptionHandler.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/utils/SvdExceptionHandler.scala
@@ -17,51 +17,51 @@ trait SvdExceptionHandler extends Actor with Logging with SvdUtils {
 
     // 2011-01-30 01:36:28 - dmilith - NOTE: txmt protocol example: txmt://open/?url=file://~/.bash_profile&line=11&column=2
 
-    /**
-     *  @author dmilith
-     *
-     *
-     */
-    def preRestart(reason: Throwable) = {
-        super.preRestart(reason, None) // XXX: CHECKME
-        log.trace("preRestart executed in %s".format(this.getClass))
-        log.error(
-            """
-Restarting Actor: (%s) cause of %s.
-Throwable details: (%s).
-            """.format(
-                this.getClass.getName, reason.getMessage,
-                reason.getStackTrace.map {
-                    traceElement =>
-                        """
-    url             - %s
-    class name      - %s
-    method name     - %s
-    file name       - %s:%s
-    native method   - %s
-                        """.format(
-                                if (traceElement.getFileName.contains("Svd")) // 2011-01-30 03:34:13 - dmilith - NOTE: all project files will include Svd prefix
-                                    "txmt://open/?url=file://%s&line=%s".format(
-                                        findFileInDir(traceElement.getFileName),
-                                        traceElement.getLineNumber
-                                    )
-                                else "No source",
-                                traceElement.getClassName,
-                                traceElement.getMethodName,
-                                traceElement.getFileName, traceElement.getLineNumber,
-                                traceElement.isNativeMethod
-                            )
-                }.mkString
-            )
-        )
+//     /**
+//      *  @author dmilith
+//      *
+//      *
+//      */
+//     def preRestart(reason: Throwable) = {
+//         super.preRestart(reason, None) // XXX: CHECKME
+//         log.trace("preRestart executed in %s".format(this.getClass))
+//         log.error(
+//             """
+// Restarting Actor: (%s) cause of %s.
+// Throwable details: (%s).
+//             """.format(
+//                 this.getClass.getName, reason.getMessage,
+//                 reason.getStackTrace.map {
+//                     traceElement =>
+//                         """
+//     url             - %s
+//     class name      - %s
+//     method name     - %s
+//     file name       - %s:%s
+//     native method   - %s
+//                         """.format(
+//                                 if (traceElement.getFileName.contains("Svd")) // 2011-01-30 03:34:13 - dmilith - NOTE: all project files will include Svd prefix
+//                                     "txmt://open/?url=file://%s&line=%s".format(
+//                                         findFileInDir(traceElement.getFileName),
+//                                         traceElement.getLineNumber
+//                                     )
+//                                 else "No source",
+//                                 traceElement.getClassName,
+//                                 traceElement.getMethodName,
+//                                 traceElement.getFileName, traceElement.getLineNumber,
+//                                 traceElement.isNativeMethod
+//                             )
+//                 }.mkString
+//             )
+//         )
 
-    }
+//     }
 
 
-    override def postStop = {
-        super.postStop
-        log.trace("postStop executed in %s".format(this.getClass))
-    }
+//     override def postStop = {
+//         super.postStop
+//         log.trace("postStop executed in %s".format(this.getClass))
+//     }
 
     override def unhandled(msg: Any){
         super.unhandled(msg)
