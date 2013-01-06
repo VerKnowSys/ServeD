08b8a27db7f1383245aefb6cf8b7800e2d0e0a60
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-01-06 17:38:47 +0100

    Little script to know how to make maven repository element from precompiled jar.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ bin/unfiltered_prebuilt_jar_put_into_maven.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn install:install-file -Dfile=/Volumes/Projects/ServeD/svd.unfiltered/target/scala-2.10/unfiltered_2.10-0.8.0-b1455.jar -DgroupId=com.verknowsys -DartifactId=unfiltered_2.10 -Dversion=0.6.4 -Dpackaging=jar
