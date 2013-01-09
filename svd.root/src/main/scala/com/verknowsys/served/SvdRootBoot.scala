68d8e8f49c52a6d4250a31e4d3dee87401bc6d3f
Michał Lipski <tallica@o2.pl>
2013-01-09 22:57:15 +0100

    It\'s Coreginx...

--- svd.root/src/main/scala/com/verknowsys/served/SvdRootBoot.scala
+++ svd.root/src/main/scala/com/verknowsys/served/SvdRootBoot.scala
@@ -23,7 +23,7 @@ class SvdRootBoot extends Logging with SvdActor {
     import akka.actor.OneForOneStrategy
     import akka.actor.SupervisorStrategy._
 
-    val systemServices = "Redis" :: "Coregnix" :: Nil // XXX: hardcoded system services
+    val systemServices = "Redis" :: "Coreginx" :: Nil // XXX: hardcoded system services
 
 
     override val supervisorStrategy = OneForOneStrategy(maxNrOfRetries = 25, withinTimeRange = 1 minute) {
