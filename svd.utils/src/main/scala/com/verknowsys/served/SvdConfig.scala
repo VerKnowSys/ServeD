be32f149d195c838d1e5d4f160eadae99d196f08
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-01-22 20:23:05 +0100

    Added missing definition of default IRC Gate identify string.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- svd.utils/src/main/scala/com/verknowsys/served/SvdConfig.scala
+++ svd.utils/src/main/scala/com/verknowsys/served/SvdConfig.scala
@@ -103,6 +103,7 @@ object SvdConfig {
 
     def defaultIRCServerName            = "216.155.130.130" // *.freenode.org
     def defaultIRCChannelName           = "#verknowsys"
+    def defaultIRCGateIdentify          = "lpmkonji" // pass for identify to NickServ for registered nick "tasks"
     def defaultVPNNetworkPrefix         = "10.10.*"
     def defaultNtpHost                  = "ntp.task.gda.pl"
     def matcherFQDN                     = """(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)"""
