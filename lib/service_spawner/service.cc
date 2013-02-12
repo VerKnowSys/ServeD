b62db0320d479904215f3a2416a61bfdead59110
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-02-12 02:57:56 +0100

    Defined rest of hook implementation (proof of concept still)
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- lib/service_spawner/service.cc
+++ lib/service_spawner/service.cc
@@ -30,11 +30,43 @@ qint64 SvdService::getUptime() {
 
 void SvdService::installSlot() {
     logDebug() << "Invoked install slot for service:" << name;
+
+    logDebug() << "Loading service igniter" << name;
+    auto config = new SvdServiceConfig(name);
+
+    logTrace() << "Launching commands:" << config->install->commands;
+    auto proc = new SvdProcess(name, "install");
+    touch(config->prefixDir() + "/.installing");
+    proc->spawnProcess(config->install->commands);
+    proc->waitForFinished();
+    proc->kill();
+
+    while (!config->serviceInstalled()) {
+        sleep(1);
+    }
+    logDebug() << "Found installed file indicator of software:" << config->softwareName << ", which is base for service:" << name;
+    QFile::remove(config->prefixDir() + "/.installing");
+
+    logTrace() << "After proc install execution:" << name;
+    delete proc;
+    delete config;
 }
 
 
 void SvdService::configureSlot() {
     logDebug() << "Invoked configure slot for service:" << name;
+    logTrace() << "Loading service igniter" << name;
+    auto config = new SvdServiceConfig(name);
+
+    logTrace() << "Launching commands:" << config->configure->commands;
+    auto proc = new SvdProcess(name, "configure");
+    proc->spawnProcess(config->configure->commands);
+
+    proc->waitForFinished();
+    proc->kill();
+    logTrace() << "After proc configure execution:" << name;
+    delete proc;
+    delete config;
 }
 
 
@@ -42,9 +74,19 @@ void SvdService::startSlot() {
     logDebug() << "Invoked start slot for service:" << name;
     uptime->start();
 
-    logDebug() << "Reloading service igniter" << name;
+    logTrace() << "Loading service igniter" << name;
     auto config = new SvdServiceConfig(name);
 
+    if (!config->serviceInstalled()) {
+        logInfo() << "Service" << name << "isn't yet installed. Proceeding with installation.";
+        installSlot();
+        logInfo() << "Service" << name << "isn't yet configured. Proceeding with configuration.";
+        configureSlot();
+    }
+    logInfo() << "Validating service" << name;
+    validateSlot(); // invoke validation before each startSlot
+
+    logInfo() << "Launching service" << name;
     logTrace() << "Launching commands:" << config->start->commands;
     auto proc = new SvdProcess(name, "start");
     proc->spawnProcess(config->start->commands);
@@ -53,7 +95,7 @@ void SvdService::startSlot() {
 
     proc->waitForFinished();
     proc->kill();
-    logTrace("After proc execution");
+    logTrace() << "After proc start execution:" << name;
     delete proc;
     delete config;
 }
@@ -61,6 +103,18 @@ void SvdService::startSlot() {
 
 void SvdService::afterStartSlot() {
     logDebug() << "Invoked after start slot for service:" << name;
+    logTrace() << "Loading service igniter" << name;
+    auto config = new SvdServiceConfig(name);
+
+    logTrace() << "Launching commands:" << config->afterStart->commands;
+    auto proc = new SvdProcess(name, "afterStart");
+    proc->spawnProcess(config->afterStart->commands);
+
+    proc->waitForFinished();
+    proc->kill();
+    logTrace() << "After proc afterStart execution:" << name;
+    delete proc;
+    delete config;
 }
 
 
@@ -72,6 +126,7 @@ void SvdService::stopSlot() {
     delete uptime;
     uptime = new QElapsedTimer();
 
+    logTrace() << "Loading service igniter" << name;
     auto config = new SvdServiceConfig(name);
     proc->spawnProcess(config->stop->commands); // invoke igniter stop, and then try to look for service.pid in prefix directory:
 
@@ -86,7 +141,7 @@ void SvdService::stopSlot() {
 
     proc->waitForFinished();
     proc->kill();
-    logTrace("After proc execution");
+    logTrace() << "After proc stop execution:" << name;
     delete proc;
     delete config;
 }
@@ -94,6 +149,18 @@ void SvdService::stopSlot() {
 
 void SvdService::afterStopSlot() {
     logDebug() << "Invoked after stop slot for service:" << name;
+    logTrace() << "Loading service igniter" << name;
+    auto config = new SvdServiceConfig(name);
+
+    logTrace() << "Launching commands:" << config->afterStop->commands;
+    auto proc = new SvdProcess(name, "afterStop");
+    proc->spawnProcess(config->afterStop->commands);
+
+    proc->waitForFinished();
+    proc->kill();
+    logTrace() << "After proc afterStop execution:" << name;
+    delete proc;
+    delete config;
 }
 
 
@@ -106,9 +173,33 @@ void SvdService::restartSlot() {
 
 void SvdService::reloadSlot() {
     logDebug() << "Invoked reload slot for service:" << name;
+    logTrace() << "Loading service igniter" << name;
+    auto config = new SvdServiceConfig(name);
+
+    logTrace() << "Launching commands:" << config->reload->commands;
+    auto proc = new SvdProcess(name, "reload");
+    proc->spawnProcess(config->reload->commands);
+
+    proc->waitForFinished();
+    proc->kill();
+    logTrace() << "After proc reload execution:" << name;
+    delete proc;
+    delete config;
 }
 
 
 void SvdService::validateSlot() {
     logDebug() << "Invoked validate slot for service:" << name;
+    logTrace() << "Loading service igniter" << name;
+    auto config = new SvdServiceConfig(name);
+
+    logTrace() << "Launching commands:" << config->validate->commands;
+    auto proc = new SvdProcess(name, "validate");
+    proc->spawnProcess(config->validate->commands);
+
+    proc->waitForFinished();
+    proc->kill();
+    logTrace() << "After proc validate execution:" << name;
+    delete proc;
+    delete config;
 }
