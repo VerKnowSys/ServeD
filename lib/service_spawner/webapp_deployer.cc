993ea45e87368d3f721f7dd055b9937f4aeac7c5
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-02-13 21:33:07 +0100

    Defined stub for webapp_deployer.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ lib/service_spawner/webapp_deployer.cc
@@ -0,0 +1,14 @@
+/**
+ *  @author dmilith
+ *
+ *   © 2013 - VerKnowSys
+ *
+ */
+
+#include "webapp_deployer.h"
+
+
+SvdWebAppDeployer::SvdWebAppDeployer(const QString& domain) {
+    logInfo() << "Performing webapp deploy for domain:" << domain;
+}
+
---
+++ lib/service_spawner/webapp_deployer.h
@@ -0,0 +1,34 @@
+/**
+ *  @author dmilith
+ *
+ *   © 2013 - VerKnowSys
+ *
+ */
+
+
+#ifndef __WEBAPP_DEPLOYER__
+#define __WEBAPP_DEPLOYER__
+
+
+#include "../kickstart/core.h"
+#include "../jsoncpp/json/json.h"
+#include "service_config.h"
+#include "utils.h"
+#include "service.h"
+#include "process.h"
+#include <QObject>
+#include <QtTest/QtTest>
+
+
+
+class SvdWebAppDeployer: QObject {
+    Q_OBJECT
+
+    public:
+        SvdWebAppDeployer(const QString& domain);
+
+
+};
+
+
+#endif
