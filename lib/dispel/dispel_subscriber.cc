5408bfc2664c5ff4982c26b86c2b1f9c0914d664
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-09-16 19:29:53 +0200

    Implemented PoC Subscriber mechanism.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ lib/dispel/dispel_subscriber.cc
@@ -0,0 +1,37 @@
+/*
+    Author: Daniel (dmilith) Dettlaff
+    based on fragment of nzmqt implementation by Johann Duscher (a.k.a. Jonny Dee)
+
+    © 2013 - VerKnowSys
+*/
+
+
+#include "dispel_subscriber.h"
+
+
+bool Subscriber::notify(QObject *obj, QEvent *event) {
+    try {
+        return notify(obj, event);
+    }
+    catch (std::exception& ex) {
+        logError() << "Exception thrown:" << ex.what();
+        return false;
+    }
+}
+
+
+void Subscriber::messageReceived(const QList<QByteArray>& message) {
+    logDebug() << "Subscriber> " << message;
+    emit pingReceived(message);
+}
+
+
+void Subscriber::startImpl() {
+    nodeUuid = readOrGenerateNodeUuid();
+    logInfo() << "Launching Subscriber with id:" << nodeUuid;
+    assert(!nodeUuid.isEmpty());
+    socket_->connectTo(address_);
+    socket_->subscribeTo(topic_);
+    logDebug() << "Subscribing to:" << address_ << "@topic:" << topic_;
+}
+
