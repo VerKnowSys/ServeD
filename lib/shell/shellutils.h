324ca8c489485bf889ed9b5181a5d3a61b681ba9
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-10-05 14:23:51 +0200

    Exported very useful function to get user dir without passwd db.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ lib/shell/shellutils.h
@@ -0,0 +1,13 @@
+
+#ifndef __SHELL_UTILS_H__
+#define __SHELL_UTILS_H__
+
+
+// #include "../globals/globals.h"
+#include "../kickstart/core.h"
+
+
+const char* gatherUserNameFromDirEntry(int uid, const char* users_home_dir);
+
+
+#endif
