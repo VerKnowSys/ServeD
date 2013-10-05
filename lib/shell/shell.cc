c8a6c2c5f387a108de76fda875afb1a2fd421b86
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-10-05 14:24:41 +0200

    Removed moved implementation from shell.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- lib/shell/shell.cc
+++ lib/shell/shell.cc
@@ -9,6 +9,7 @@
 
 
 #include "../kickstart/core.h"
+#include "../shell/shellutils.h"
 
 
 static struct termios saveTermios;
@@ -53,44 +54,6 @@ string getUserHomeDirAndAskForName(int uid) {
     }
 
     return ss.str();
-}
-
-
-const char* gatherUserNameFromDirEntry(int uid, const char* users_home_dir) {
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir(users_home_dir)) != NULL) {
-        /* print all the files and directories within directory */
-        while ((ent = readdir(dir)) != NULL) {
-            if ((strcmp(ent->d_name, ".") == 0) || (strcmp(ent->d_name, "..") == 0)) continue;
-            // only if owner of given directory is set to current runtime uid…
-            struct stat buffer;
-            int status;
-            char *fname = ent->d_name;
-            stringstream hd;
-            stringstream hd2;
-            hd << fname;
-            hd2 << users_home_dir << "/" << fname;
-            status = stat(hd2.str().c_str(), &buffer);
-            if (buffer.st_uid == uid) {
-                #ifdef DEVEL
-                    cerr << "UID EQUALS current uid for " << fname << endl;
-                #endif
-                closedir(dir);
-                return hd.str().c_str();
-            }
-            #ifdef DEVEL
-                cerr << "UID of file: " << fname << " is " << buffer.st_uid << endl;
-            #endif
-        }
-        closedir(dir);
-        return NULL;
-    } else {
-        /* could not open directory */
-        perror ("");
-        cerr << "Failed to find required home directory. Cannot continue" << endl;
-        exit(EXIT_FAILURE);
-    }
 }
 
 
